a6a16785eabebf038d71e987646092f59a0f3318
Vitaliy Rodyna <ukrdao@gmail.com>
2022-02-04 07:04:58 +0200

    added task 3 with tests

---
+++ src/test/java/com/task3/Solution3Test.java
@@ -0,0 +1,26 @@
+package com.task3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution3Test {
+	
+     Solution3 s = new Solution3();
+     
+    @Test
+    public void test1() {      
+        assertEquals(3, s.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    @Test
+    public void test2() {  
+        assertEquals(1, s.lengthOfLongestSubstring("bbbbb"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(3, s.lengthOfLongestSubstring("pwwkew"));
+    }
+}
+
