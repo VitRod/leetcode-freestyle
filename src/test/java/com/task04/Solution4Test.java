b9c129d3f125204302da7cfe42a7dfc3a2d36d0a
Vitaliy Rodyna <ukrdao@gmail.com>
2022-02-05 03:07:51 +0200

    changed imports  in tests

--- src/test/java/com/task04/Solution4Test.java
+++ src/test/java/com/task04/Solution4Test.java
@@ -1,20 +1,20 @@
 package com.task04;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class Solution4Test {
-	
+
 	Solution4 s = new Solution4();
 
-	@Test 
-    public void test1() {
-        assertEquals(2.0, s.findMedianSortedArrays(new int[] {1,3}, new int[] {2}));
-    }
+	@Test
+	public void test1() {
+		assertEquals(2.0, s.findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+	}
 
-    @Test
-    public void test2() {
-        assertEquals(2.5, s.findMedianSortedArrays(new int[] {1,2}, new int[] {3,4}));
-    }
+	@Test
+	public void test2() {
+		assertEquals(2.5, s.findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+	}
 }
