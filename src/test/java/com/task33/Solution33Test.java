b8421803af0625f66d9d731f363c069f5aee2c93
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-17 12:39:41 +0200

    generated test stub

---
+++ src/test/java/com/task33/Solution33Test.java
@@ -0,0 +1,14 @@
+package com.task33;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution33Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
