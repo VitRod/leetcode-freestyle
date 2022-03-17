de60337b136178d55bb4a9e6afd72efe1d79c168
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-17 12:36:51 +0200

    formatting

--- src/main/java/com/task33/Solution33.java
+++ src/main/java/com/task33/Solution33.java
@@ -15,8 +15,6 @@ package com.task33;
 
 //You must write an algorithm with O(log n) runtime complexity.
 
-
-
 //Example 1:
 
 //Input: nums = [4,5,6,7,0,1,2], target = 0
@@ -30,7 +28,6 @@ package com.task33;
 //Input: nums = [1], target = 0
 //Output: -1
 
-
 //Constraints:
 
 //1 <= nums.length <= 5000
@@ -39,44 +36,43 @@ package com.task33;
 //nums is an ascending array that is possibly rotated.
 //-104 <= target <= 104
 
-
 public class Solution33 {
-	
+
 	public int search(int[] nums, int target) {
 		// `left` is the index of the first element of the array
-		        int left = 0; 
+		int left = 0;
 		// `right` is the index of the last element of the array
-		        int right = nums.length - 1; 
-		        // `left` and `right` are not equal
-		        while (left <= right) { 
-		    // `mid` is the index of the middle element of the array
-		            int mid = left + (right - left) / 2; 
-		     // `target` is in the array
-		            if (nums[mid] == target) { 
-		    // return the index of `target`
-		                return mid; 
-		            }
-		    // `left` and `mid` are in the ascending order
-		            if (nums[left] <= nums[mid]) { 
-		    // `target` is in the ascending order
-		                if (nums[left] <= target && target < nums[mid]) { 
-		    // `right` is the index of the last element of the array
-		                    right = mid - 1; 
-		                } else {
-		    // `left` is the index of the first element of the array
-		                    left = mid + 1; 
-		                }
-		            } else {
-		    // `target` is in the ascending order
-		                if (nums[mid] < target && target <= nums[right]) { 
-		    // `left` is the index of the first element of the array
-		                    left = mid + 1; 
-		                } else {
-		    // `right` is the index of the last element of the array
-		                    right = mid - 1; 
-		                }
-		            }
-		        }
-		        return -1; // `target` is not in the array
-		    }
+		int right = nums.length - 1;
+		// `left` and `right` are not equal
+		while (left <= right) {
+			// `mid` is the index of the middle element of the array
+			int mid = left + (right - left) / 2;
+			// `target` is in the array
+			if (nums[mid] == target) {
+				// return the index of `target`
+				return mid;
+			}
+			// `left` and `mid` are in the ascending order
+			if (nums[left] <= nums[mid]) {
+				// `target` is in the ascending order
+				if (nums[left] <= target && target < nums[mid]) {
+					// `right` is the index of the last element of the array
+					right = mid - 1;
+				} else {
+					// `left` is the index of the first element of the array
+					left = mid + 1;
+				}
+			} else {
+				// `target` is in the ascending order
+				if (nums[mid] < target && target <= nums[right]) {
+					// `left` is the index of the first element of the array
+					left = mid + 1;
+				} else {
+					// `right` is the index of the last element of the array
+					right = mid - 1;
+				}
+			}
 		}
+		return -1; // `target` is not in the array
+	}
+}
