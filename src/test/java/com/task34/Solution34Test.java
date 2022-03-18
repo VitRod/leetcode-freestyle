f307230636d8f8174001cae08ae04e535bf568be
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-18 12:12:35 +0200

    generated test stub

---
+++ src/test/java/com/task34/Solution34Test.java
@@ -0,0 +1,14 @@
+package com.task34;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution34Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
