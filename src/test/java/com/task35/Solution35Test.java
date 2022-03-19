430c4fb60402d2c750420250b6f13323af5e7b0e
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-19 10:58:41 +0200

    added tests itself

--- src/test/java/com/task35/Solution35Test.java
+++ src/test/java/com/task35/Solution35Test.java
@@ -6,9 +6,32 @@ import org.junit.jupiter.api.Test;
 
 class Solution35Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution35 solution = new Solution35();
+    
+    @Test
+    public void test1() {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        int expected = 2;
+        int actual = solution.searchInsert(nums, target);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void test2() {
+        int[] nums = {1,3,5,6};
+        int target = 2;
+        int expected = 1;
+        int actual = solution.searchInsert(nums, target);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        int expected = 4;
+        int actual = solution.searchInsert(nums, target);
+        assertEquals(expected, actual);
+    }
 }
