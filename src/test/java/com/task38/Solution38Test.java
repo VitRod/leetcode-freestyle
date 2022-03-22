24e57d041506e4e6bffb95d797a628fd0c02f1db
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-22 07:53:01 +0200

    added tests

--- src/test/java/com/task38/Solution38Test.java
+++ src/test/java/com/task38/Solution38Test.java
@@ -5,10 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Solution38Test {
+	
+	  Solution38 solution = new Solution38();
+	   
+	    @Test
+	    public void test1() {
+	        assertEquals("1", solution.countAndSay(1));
+	    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	    @Test
+	    public void test2() {
+	        assertEquals("11", solution.countAndSay(2));
+	    }
+
+	    @Test
+	    public void test3() {
+	        assertEquals("21", solution.countAndSay(3));
+	    }
 	}
-
-}
