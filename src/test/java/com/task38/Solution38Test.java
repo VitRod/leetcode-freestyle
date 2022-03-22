0f6b7992f750a541a3d896cff209a19a4ae3362f
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-22 07:52:01 +0200

    generated test stub

---
+++ src/test/java/com/task38/Solution38Test.java
@@ -0,0 +1,14 @@
+package com.task38;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution38Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
