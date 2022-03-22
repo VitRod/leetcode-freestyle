a1770eb954a5fefb017f6ef539457a52615933fe
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-22 07:49:29 +0200

    added solution itself

--- src/main/java/com/task38/Solution38.java
+++ src/main/java/com/task38/Solution38.java
@@ -41,5 +41,25 @@ package com.task38;
 //1 <= n <= 30
 
 public class Solution38 {
-
+	
+	public String countAndSay(int n) {
+        String str = "1";
+        while (--n > 0) {
+            int times = 1;
+            StringBuilder sb = new StringBuilder();
+            char[] chars = str.toCharArray();
+            int len = chars.length;
+            for (int j = 1; j < len; j++) {
+                if (chars[j - 1] == chars[j]) {
+                    times++;
+                } else {
+                    sb.append(times).append(chars[j - 1]);
+                    times = 1;
+                }
+            }
+            str = sb.append(times).append(chars[len - 1]).toString();
+        }
+        return str;
+    }
 }
+
