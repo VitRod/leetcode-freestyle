92f7a66e3926c332e2683d9058937e0f9ec61607
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-25 06:02:35 +0200

    added solution

--- src/main/java/com/task41/Solution41.java
+++ src/main/java/com/task41/Solution41.java
@@ -20,7 +20,6 @@ package com.task41;
 //Input: nums = [7,8,9,11,12]
 //Output: 1
 
-
 //Constraints:
 
 //1 <= nums.length <= 5 * 105
@@ -28,4 +27,21 @@ package com.task41;
 
 public class Solution41 {
 
+	public int firstMissingPositive(int[] nums) {
+
+		int n = nums.length; // `n` is the length of the array
+		for (int i = 0; i < n; i++) { // `i` is the index of the array
+			// `nums[i]` is the value of the array at index `i`
+			while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+				int temp = nums[i]; // `temp` is the value of the array at index `i`
+				nums[i] = nums[temp - 1]; // `nums[i]` is the value of the array at index `temp-1`
+				nums[temp - 1] = temp; // `nums[temp-1]` is the value of the array at index `i`
+			}
+		}
+		for (int i = 0; i < n; i++) { // `i` is the index of the array
+			if (nums[i] != i + 1) // `nums[i]` is the value of the array at index `i`
+				return i + 1; // `i+1` is the smallest missing positive integer
+		}
+		return n + 1; // `n+1` is the smallest missing positive integer
+	}
 }
