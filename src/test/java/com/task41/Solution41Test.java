d7e0db5fd739d9cc915262321b50c54ecef57311
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-25 06:06:52 +0200

    formatting

--- src/test/java/com/task41/Solution41Test.java
+++ src/test/java/com/task41/Solution41Test.java
@@ -7,28 +7,28 @@ import org.junit.jupiter.api.Test;
 class Solution41Test {
 
 	Solution41 solution = new Solution41();
-	   
-    @Test
-    public void test1() {
-        int[] nums = {1,2,0};
-        int expected = 3;
-        int actual = solution.firstMissingPositive(nums);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    public void test2() {
-        int[] nums = {3,4,-1,1};
-        int expected = 2;
-        int actual = solution.firstMissingPositive(nums);
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void test1() {
+		int[] nums = { 1, 2, 0 };
+		int expected = 3;
+		int actual = solution.firstMissingPositive(nums);
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void test3() {
-        int[] nums = {7,8,9,11,12};
-        int expected = 1;
-        int actual = solution.firstMissingPositive(nums);
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void test2() {
+		int[] nums = { 3, 4, -1, 1 };
+		int expected = 2;
+		int actual = solution.firstMissingPositive(nums);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test3() {
+		int[] nums = { 7, 8, 9, 11, 12 };
+		int expected = 1;
+		int actual = solution.firstMissingPositive(nums);
+		assertEquals(expected, actual);
+	}
 }
