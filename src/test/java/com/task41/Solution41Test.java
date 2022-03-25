8dfa370a9adc5f82e068d103eecae87d2e0aade7
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-25 06:05:21 +0200

    added tests

--- src/test/java/com/task41/Solution41Test.java
+++ src/test/java/com/task41/Solution41Test.java
@@ -6,9 +6,29 @@ import org.junit.jupiter.api.Test;
 
 class Solution41Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution41 solution = new Solution41();
+	   
+    @Test
+    public void test1() {
+        int[] nums = {1,2,0};
+        int expected = 3;
+        int actual = solution.firstMissingPositive(nums);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void test2() {
+        int[] nums = {3,4,-1,1};
+        int expected = 2;
+        int actual = solution.firstMissingPositive(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        int[] nums = {7,8,9,11,12};
+        int expected = 1;
+        int actual = solution.firstMissingPositive(nums);
+        assertEquals(expected, actual);
+    }
 }
