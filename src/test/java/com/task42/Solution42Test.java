28165c11d41290ff0ea55f4997d04b002b22b79f
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-26 07:23:53 +0200

    added test

--- src/test/java/com/task42/Solution42Test.java
+++ src/test/java/com/task42/Solution42Test.java
@@ -6,9 +6,21 @@ import org.junit.jupiter.api.Test;
 
 class Solution42Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	 Solution42 solution = new Solution42();
+	   
+	    @Test
+	    public void test1() {
+	        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+	        int expected = 6;
+	        int actual = solution.trap(height);
+	        assertEquals(expected, actual);
+	    }
 
-}
+	    @Test
+	    public void test2() {
+	        int[] height = {4,2,0,3,2,5};
+	        int expected = 9;
+	        int actual = solution.trap(height);
+	        assertEquals(expected, actual);
+	    }
+	}
