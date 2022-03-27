5ff95267a9a99456483e8ebe0effaf5750b8c015
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-27 07:31:04 +0300

    formatting

--- src/test/java/com/task43/Solution43Test.java
+++ src/test/java/com/task43/Solution43Test.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class Solution43Test {
 
-	 Solution43 solution = new Solution43();
-	   
-	    @Test
-	    public void test1() {
-	        assertEquals("6", solution.multiply("2", "3"));
-	    }
+	Solution43 solution = new Solution43();
 
-	    @Test
-	    public void test2() {
-	        assertEquals("56088", solution.multiply("123", "456"));
-	    }
+	@Test
+	public void test1() {
+		assertEquals("6", solution.multiply("2", "3"));
 	}
+
+	@Test
+	public void test2() {
+		assertEquals("56088", solution.multiply("123", "456"));
+	}
+}
