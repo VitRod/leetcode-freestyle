d957372a5e4f8cc22eaa434366db38ce45083209
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-27 07:24:26 +0300

    added solution

--- src/main/java/com/task43/Solution43.java
+++ src/main/java/com/task43/Solution43.java
@@ -29,4 +29,20 @@ package com.task43;
 
 public class Solution43 {
 
+	public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + res[p2];
+                res[p1] += sum / 10;
+                res[p2] = (sum) % 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int p : res) if (!(sb.length() == 0 && p == 0)) sb.append(p);
+        return sb.toString();
+    }
 }
