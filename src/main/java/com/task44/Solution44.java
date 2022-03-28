f49bbfe5d9c34e5f03f55a93e2a3d8525cb11328
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-28 11:11:05 +0300

    formatting

--- src/main/java/com/task44/Solution44.java
+++ src/main/java/com/task44/Solution44.java
@@ -10,8 +10,6 @@ package com.task44;
 //'*' Matches any sequence of characters (including the empty sequence).
 //The matching should cover the entire input string (not partial).
 
-
-
 //Example 1:
 
 //Input: s = "aa", p = "a"
@@ -28,36 +26,33 @@ package com.task44;
 //Output: false
 //Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
 
-
 //Constraints:
 
 //0 <= s.length, p.length <= 2000
 //s contains only lowercase English letters.
 //p contains only lowercase English letters, '?' or '*'.
 
-
 public class Solution44 {
 
 	public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= n; i++) {
-            if (p.charAt(i - 1) == '*') {
-                dp[0][i] = dp[0][i - 1];
-            }
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-            }
-        }
-        return dp[m][n];
-    }
+		int m = s.length();
+		int n = p.length();
+		boolean[][] dp = new boolean[m + 1][n + 1];
+		dp[0][0] = true;
+		for (int i = 1; i <= n; i++) {
+			if (p.charAt(i - 1) == '*') {
+				dp[0][i] = dp[0][i - 1];
+			}
+		}
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (p.charAt(j - 1) == '*') {
+					dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+				} else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1];
+				}
+			}
+		}
+		return dp[m][n];
+	}
 }
-
