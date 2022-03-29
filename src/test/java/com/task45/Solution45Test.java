cae3affb5ddc02ca651ca20ddbf8a0c3e91e49d5
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-29 07:43:05 +0300

    added solution

--- src/test/java/com/task45/Solution45Test.java
+++ src/test/java/com/task45/Solution45Test.java
@@ -6,9 +6,21 @@ import org.junit.jupiter.api.Test;
 
 class Solution45Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution45 solution = new Solution45();
+	 
+    @Test
+    public void test1() {
+        int[] nums = {2, 3, 1, 1, 4};
+        int expected = 2;
+        int actual = solution.jump(nums);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void test2() {
+        int[] nums = {2, 3, 0, 1, 4};
+        int expected = 2;
+        int actual = solution.jump(nums);
+        assertEquals(expected, actual);
+    }
 }
