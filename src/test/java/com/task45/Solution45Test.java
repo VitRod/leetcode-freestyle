bece11ea248acd97a87c60b76b38a184fd6c6fa0
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-29 07:42:14 +0300

    generated test stub

---
+++ src/test/java/com/task45/Solution45Test.java
@@ -0,0 +1,14 @@
+package com.task45;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution45Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
