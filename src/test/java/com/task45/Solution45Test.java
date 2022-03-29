55540d675dbe8fa97dd40e4abd2fda90d7ae2e93
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-29 07:43:45 +0300

    formatting

--- src/test/java/com/task45/Solution45Test.java
+++ src/test/java/com/task45/Solution45Test.java
@@ -7,20 +7,20 @@ import org.junit.jupiter.api.Test;
 class Solution45Test {
 
 	Solution45 solution = new Solution45();
-	 
-    @Test
-    public void test1() {
-        int[] nums = {2, 3, 1, 1, 4};
-        int expected = 2;
-        int actual = solution.jump(nums);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    public void test2() {
-        int[] nums = {2, 3, 0, 1, 4};
-        int expected = 2;
-        int actual = solution.jump(nums);
-        assertEquals(expected, actual);
-    }
+	@Test
+	public void test1() {
+		int[] nums = { 2, 3, 1, 1, 4 };
+		int expected = 2;
+		int actual = solution.jump(nums);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void test2() {
+		int[] nums = { 2, 3, 0, 1, 4 };
+		int expected = 2;
+		int actual = solution.jump(nums);
+		assertEquals(expected, actual);
+	}
 }
