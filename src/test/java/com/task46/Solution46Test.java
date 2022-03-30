f2a327ac41a2341e17d9c03d04352c771e8e2748
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-30 05:14:33 +0300

    stub

--- src/test/java/com/task46/Solution46Test.java
+++ src/test/java/com/task46/Solution46Test.java
@@ -1,5 +1,14 @@
 package com.task46;
 
-public class Solution46Test {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution46Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
