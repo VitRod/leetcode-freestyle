6cefd28866e4de3992ca4e5091410738445facd3
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-30 05:08:41 +0300

    added task comments

--- src/main/java/com/task46/Solution46.java
+++ src/main/java/com/task46/Solution46.java
@@ -1,5 +1,36 @@
 package com.task46;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 46. Permutations
+// Medium
+
+// Given an array nums of distinct integers, return all the possible permutations. 
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+// Example 3:
+
+// Input: nums = [1]
+// Output: [[1]]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// All the integers of nums are unique.
+
+
 public class Solution46 {
 
 }
