02d1fa63967a3aac33b1ed55788c866c6d6d680a
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-31 05:41:32 +0300

    added solution with comments

--- src/main/java/com/task47/Solution47.java
+++ src/main/java/com/task47/Solution47.java
@@ -31,5 +31,34 @@ import java.util.List;
 // -10 <= nums[i] <= 10
 
 public class Solution47 {
+	
+	public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>(); // result
+        if (nums == null || nums.length == 0) {  // if nums is empty, return empty result
+            return result;
+        }
+        Arrays.sort(nums); // sort nums
+        boolean[] visited = new boolean[nums.length]; // visited
+        List<Integer> list = new ArrayList<>(); // list
+        helper(nums, visited, list, result); // helper
+        return result; // return result
+    }
 
+    private void helper(int[] nums, boolean[] visited, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == nums.length) { // if list is full, add list to result
+            result.add(new ArrayList<>(list)); // add list to result
+            return; // return
+        }
+        for (int i = 0; i < nums.length; i++) { // for each nums[i]
+            // if visited[i] or nums[i] == nums[i - 1] and not visited[i - 1]
+            if (visited[i] || (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])) { 
+                continue; // continue
+            }
+            visited[i] = true; // set visited[i] to true
+            list.add(nums[i]); // add nums[i] to list
+            helper(nums, visited, list, result); // helper
+            list.remove(list.size() - 1); // remove nums[i] from list
+            visited[i] = false; // set visited[i] to false
+        }
+    }
 }
