1b97c978dfe657b9ffb235ab3b112991efd74536
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-31 05:46:41 +0300

    formatting

--- src/test/java/com/task47/Solution47Test.java
+++ src/test/java/com/task47/Solution47Test.java
@@ -9,30 +9,23 @@ import org.junit.jupiter.api.Test;
 
 class Solution47Test {
 
-	 Solution47 solution = new Solution47();
-	 
-	    @Test
-	    public void test1() {
-	        int[] nums = { 1, 1, 2 };
-	        List<List<Integer>> expected = Arrays.asList(
-	                Arrays.asList(1, 1, 2),
-	                Arrays.asList(1, 2, 1),
-	                Arrays.asList(2, 1, 1));
-	        List<List<Integer>> actual = solution.permuteUnique(nums);
-	        assertEquals(expected, actual);
-	    }
+	Solution47 solution = new Solution47();
 
-	    @Test
-	    public void test2() {
-	        int[] nums = { 1, 2, 3 };
-	        List<List<Integer>> expected = Arrays.asList(
-	                Arrays.asList(1, 2, 3),
-	                Arrays.asList(1, 3, 2),
-	                Arrays.asList(2, 1, 3),
-	                Arrays.asList(2, 3, 1),
-	                Arrays.asList(3, 1, 2),
-	                Arrays.asList(3, 2, 1));
-	        List<List<Integer>> actual = solution.permuteUnique(nums);
-	        assertEquals(expected, actual);
-	    }
+	@Test
+	public void test1() {
+		int[] nums = { 1, 1, 2 };
+		List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 1, 2), Arrays.asList(1, 2, 1),
+				Arrays.asList(2, 1, 1));
+		List<List<Integer>> actual = solution.permuteUnique(nums);
+		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void test2() {
+		int[] nums = { 1, 2, 3 };
+		List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2),
+				Arrays.asList(2, 1, 3), Arrays.asList(2, 3, 1), Arrays.asList(3, 1, 2), Arrays.asList(3, 2, 1));
+		List<List<Integer>> actual = solution.permuteUnique(nums);
+		assertEquals(expected, actual);
+	}
+}
