6aae7775a223ccff3ab44aba52cc196ac08b39dd
Vitaliy Rodyna <ukrdao@gmail.com>
2022-03-31 05:35:34 +0300

    added task comments

--- src/main/java/com/task47/Solution47.java
+++ src/main/java/com/task47/Solution47.java
@@ -1,5 +1,35 @@
 package com.task47;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 47. Permutations II
+// Medium
+
+// Given a collection of numbers, nums, that might contain duplicates, 
+// return all possible unique permutations in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output:
+// [[1,1,2],
+//  [1,2,1],
+//  [2,1,1]]
+// Example 2:
+
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 8
+// -10 <= nums[i] <= 10
+
 public class Solution47 {
 
 }
