0a4b07aa406b7d7034acf1754203790a07347133
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-01 05:26:53 +0300

    added tests

--- src/test/java/com/task48/Solution48Test.java
+++ src/test/java/com/task48/Solution48Test.java
@@ -2,13 +2,27 @@ package com.task48;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Solution48Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution48 solution = new Solution48();
+	 
+    @Test
+    public void test1() {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] expected = {{7,4,1},{8,5,2},{9,6,3}};
+        solution.rotate(matrix);
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
+    }
 
+    @Test
+    public void test2() {
+        int[][] matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int[][] expected = {{15,13,2,5},{14,3,4,1},{12,6,8,9},{16,7,10,11}};
+        solution.rotate(matrix);
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(matrix));
+    }
 }
