38a47c4dc9dbb338c89f611d7f0503c0df18a3da
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-02 08:02:19 +0300

    added test

--- src/test/java/com/task49/Solution49Test.java
+++ src/test/java/com/task49/Solution49Test.java
@@ -2,13 +2,22 @@ package com.task49;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Solution49Test {
+	
+	Solution49 solution = new Solution49();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    public void test1() {
+        String[] strs = new String[] { "" };
+        List<List<String>> expected = Arrays.asList(
+                Arrays.asList("")
+        );
+        List<List<String>> actual = solution.groupAnagrams(strs);
+        assertEquals(expected, actual);
+    }
 }
