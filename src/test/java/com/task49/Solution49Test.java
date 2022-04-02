a82d7a37dd40aa78c862e717d59f7d91b4cb1322
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-02 08:02:52 +0300

    formatting

--- src/test/java/com/task49/Solution49Test.java
+++ src/test/java/com/task49/Solution49Test.java
@@ -8,16 +8,14 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Solution49Test {
-	
+
 	Solution49 solution = new Solution49();
 
 	@Test
-    public void test1() {
-        String[] strs = new String[] { "" };
-        List<List<String>> expected = Arrays.asList(
-                Arrays.asList("")
-        );
-        List<List<String>> actual = solution.groupAnagrams(strs);
-        assertEquals(expected, actual);
-    }
+	public void test1() {
+		String[] strs = new String[] { "" };
+		List<List<String>> expected = Arrays.asList(Arrays.asList(""));
+		List<List<String>> actual = solution.groupAnagrams(strs);
+		assertEquals(expected, actual);
+	}
 }
