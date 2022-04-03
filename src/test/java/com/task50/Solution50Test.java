34ba62ea7719a1f34360a996b2f1775f10fbf959
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-03 06:59:03 +0300

    formatting

--- src/test/java/com/task50/Solution50Test.java
+++ src/test/java/com/task50/Solution50Test.java
@@ -4,23 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-
 class Solution50Test {
 
 	Solution50 solution = new Solution50();
-	 
-    @Test
-    public void test1() {
-        assertEquals(1024.00000, solution.myPow(2.00000, 10), 0.00001);
-    }
 
-    @Test
-    public void test2() {
-        assertEquals(9.26100, solution.myPow(2.10000, 3), 0.00001);
-    }
+	@Test
+	public void test1() {
+		assertEquals(1024.00000, solution.myPow(2.00000, 10), 0.00001);
+	}
 
-    @Test
-    public void test3() {
-        assertEquals(0.25000, solution.myPow(2.00000, -2), 0.00001);
-    }
+	@Test
+	public void test2() {
+		assertEquals(9.26100, solution.myPow(2.10000, 3), 0.00001);
+	}
+
+	@Test
+	public void test3() {
+		assertEquals(0.25000, solution.myPow(2.00000, -2), 0.00001);
+	}
 }
