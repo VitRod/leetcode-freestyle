047f6b23e69ae36984253343baa4f22f98edc4b6
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-04 08:12:50 +0300

    formatting

--- src/main/java/com/task53/Solution53.java
+++ src/main/java/com/task53/Solution53.java
@@ -8,8 +8,6 @@ package com.task53;
 
 //A subarray is a contiguous part of an array.
 
-
-
 //Example 1:
 
 //Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
@@ -24,12 +22,10 @@ package com.task53;
 //Input: nums = [5,4,-1,7,8]
 //Output: 23
 
-
 //Constraints:
 
 //1 <= nums.length <= 105
 //-104 <= nums[i] <= 104
-
 
 //Follow up: If you have figured out the O(n) solution,
 //try coding another solution using the divide and conquer approach, which is more subtle.
@@ -37,19 +33,18 @@ package com.task53;
 public class Solution53 {
 
 	public int maxSubArray(int[] nums) {
-        int max = nums[0]; // max sum
-        int sum = 0; // current sum
-        for (int i = 0; i < nums.length; i++) { // for each element
-            sum += nums[i]; // add to current sum
-            if (sum > max) { // if current sum is greater than max sum
-                max = sum; // update max sum
-            }
-            if (sum < 0) { // if current sum is less than 0
-                sum = 0; // reset sum to 0
-            }
-        }
-        return max; //  return max sum
+		int max = nums[0]; // max sum
+		int sum = 0; // current sum
+		for (int i = 0; i < nums.length; i++) { // for each element
+			sum += nums[i]; // add to current sum
+			if (sum > max) { // if current sum is greater than max sum
+				max = sum; // update max sum
+			}
+			if (sum < 0) { // if current sum is less than 0
+				sum = 0; // reset sum to 0
+			}
+		}
+		return max; // return max sum
 
-    }
+	}
 }
-
