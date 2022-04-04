8b15d2ca595311d101c9dd5cd269dc2ca4b7e59b
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-04 08:11:41 +0300

    added tests

--- src/test/java/com/task53/Solution53Test.java
+++ src/test/java/com/task53/Solution53Test.java
@@ -1,5 +1,30 @@
 package com.task53;
 
-public class Solution53Test {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-}
+import org.junit.jupiter.api.Test;
+
+public class Solution53Test {
+	
+	 Solution53 s = new Solution53();
+
+	 @Test
+	    public void test1() {	       
+	        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+	        assertEquals(6, s.maxSubArray(nums));
+	    }
+
+	    @Test
+	    public void test2() {
+	        int[] nums = {1};
+	        assertEquals(1, s.maxSubArray(nums));
+	    }
+
+	    @Test
+	    public void test3() {
+	        int[] nums = {5,4,-1,7,8};
+	        assertEquals(23, s.maxSubArray(nums));
+	    }
+	}
+
+
