92aa3cf044cc2816b336d3c8d56582d7b1d94c26
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-04 08:12:16 +0300

    formatting

--- src/test/java/com/task53/Solution53Test.java
+++ src/test/java/com/task53/Solution53Test.java
@@ -5,26 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class Solution53Test {
-	
-	 Solution53 s = new Solution53();
 
-	 @Test
-	    public void test1() {	       
-	        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-	        assertEquals(6, s.maxSubArray(nums));
-	    }
+	Solution53 s = new Solution53();
 
-	    @Test
-	    public void test2() {
-	        int[] nums = {1};
-	        assertEquals(1, s.maxSubArray(nums));
-	    }
-
-	    @Test
-	    public void test3() {
-	        int[] nums = {5,4,-1,7,8};
-	        assertEquals(23, s.maxSubArray(nums));
-	    }
+	@Test
+	public void test1() {
+		int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		assertEquals(6, s.maxSubArray(nums));
 	}
 
+	@Test
+	public void test2() {
+		int[] nums = { 1 };
+		assertEquals(1, s.maxSubArray(nums));
+	}
 
+	@Test
+	public void test3() {
+		int[] nums = { 5, 4, -1, 7, 8 };
+		assertEquals(23, s.maxSubArray(nums));
+	}
+}
