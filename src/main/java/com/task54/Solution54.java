ac16bb99364d077ce4b39dd01efc7e8f06f095bb
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-05 08:22:47 +0300

    changes

--- src/main/java/com/task54/Solution54.java
+++ src/main/java/com/task54/Solution54.java
@@ -11,6 +11,7 @@ import java.util.List;
 // Example 1:
 // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 // Output: [1,2,3,6,9,8,7,4,5]
+
 // Example 2:
 
 // Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
