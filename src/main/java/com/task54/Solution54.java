65f9f2a0efe3f4f996b1dc7a9dd9d19db2b29f14
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-05 08:16:05 +0300

    little  changes

--- src/main/java/com/task54/Solution54.java
+++ src/main/java/com/task54/Solution54.java
@@ -9,7 +9,6 @@ import java.util.List;
 // Given an m x n matrix, return all elements of the matrix in spiral order.
 
 // Example 1:
-
 // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 // Output: [1,2,3,6,9,8,7,4,5]
 // Example 2:
