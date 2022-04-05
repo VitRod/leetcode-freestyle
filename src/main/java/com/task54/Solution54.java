671868199a04e208145e29dd6106694654a5c512
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-05 08:14:33 +0300

    added solution

--- src/main/java/com/task54/Solution54.java
+++ src/main/java/com/task54/Solution54.java
@@ -33,4 +33,26 @@ import java.util.List;
 
 public class Solution54 {
 
+	public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>(); // initialize result list
+        if (matrix.length == 0) return res; // if matrix is empty, return empty list
+        int m = matrix.length, n = matrix[0].length; // initialize m and n
+        int[][] visited = new int[m][n]; // initialize visited matrix
+        int[] dx = {0, 1, 0, -1}; // initialize dx and dy
+        int[] dy = {1, 0, -1, 0};
+        int x = 0, y = 0, d = 0; // initialize x, y, d
+        for (int i = 0; i < m * n; i++) { // loop until all elements are visited
+            res.add(matrix[x][y]); // add current element to result list
+            visited[x][y] = 1; // mark current element as visited
+            int nx = x + dx[d], ny = y + dy[d]; // calculate next x and y
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n || visited[nx][ny] == 1) { // if next x or y is out of bound or next element is visited
+                d = (d + 1) % 4; // change direction
+                nx = x + dx[d]; // calculate next x
+                ny = y + dy[d]; // calculate next y
+            }
+            x = nx; // update x
+            y = ny; // update y
+        }
+        return res; // return result list
+    } 
 }
