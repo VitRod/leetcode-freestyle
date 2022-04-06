2dc55c24d5fc3a087b50ad013aea9e41514ccdfe
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-06 07:09:00 +0300

    added tests

--- src/test/java/com/task55/Solution55Test.java
+++ src/test/java/com/task55/Solution55Test.java
@@ -6,9 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class Solution55Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution55 s = new Solution55();
 
+    @Test
+    public void test1() {
+        int[] nums = {2,3,1,1,4};
+        assertEquals(true, s.canJump(nums));
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {3,2,1,0,4};
+        assertEquals(false, s.canJump(nums));
+    }
 }
