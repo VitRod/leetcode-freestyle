8e9de573bff76c306cffb60ccd5a54d31d163149
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-08 07:13:49 +0300

    added solution

--- src/main/java/com/task58/Solution58.java
+++ src/main/java/com/task58/Solution58.java
@@ -34,5 +34,11 @@ package com.task58;
 //There will be at least one word in s.
 
 public class Solution58 {
-
+	public int lengthOfLastWord(String s) {
+        int p = s.length() - 1;
+        while(p >=0 && s.charAt(p) == ' ') p--;
+        int end = p;
+        while(p >= 0 && s.charAt(p) != ' ') p--;
+       return end - p;
+    }
 }
