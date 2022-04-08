211b5a3be9ca6cddae38baab61ee5ad70c49e852
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-08 07:18:09 +0300

    formatting

--- src/test/java/com/task58/Solution58Test.java
+++ src/test/java/com/task58/Solution58Test.java
@@ -8,18 +8,18 @@ class Solution58Test {
 
 	Solution58 s = new Solution58();
 
-    @Test
-    public void test1() {
-        assertEquals(5, s.lengthOfLastWord("Hello World"));
-    }
+	@Test
+	public void test1() {
+		assertEquals(5, s.lengthOfLastWord("Hello World"));
+	}
 
-    @Test
-    public void test2() {
-        assertEquals(4, s.lengthOfLastWord("   fly me   to   the moon  "));
-    }
+	@Test
+	public void test2() {
+		assertEquals(4, s.lengthOfLastWord("   fly me   to   the moon  "));
+	}
 
-    @Test
-    public void test3() {
-        assertEquals(6, s.lengthOfLastWord("luffy is still joyboy"));
-    }
+	@Test
+	public void test3() {
+		assertEquals(6, s.lengthOfLastWord("luffy is still joyboy"));
+	}
 }
