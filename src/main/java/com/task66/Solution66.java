277f26d5eb492c4867e350fd63cf78f1e2755fa0
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-10 08:39:31 +0300

    formatting

--- src/main/java/com/task66/Solution66.java
+++ src/main/java/com/task66/Solution66.java
@@ -10,8 +10,6 @@ package com.task66;
 
 //Increment the large integer by one and return the resulting array of digits.
 
-
-
 //Example 1:
 
 //Input: digits = [1,2,3]
@@ -34,33 +32,32 @@ package com.task66;
 //Incrementing by one gives 9 + 1 = 10.
 //Thus, the result should be [1,0].
 
-
 //Constraints:
 
 //1 <= digits.length <= 100
 //0 <= digits[i] <= 9
 //digits does not contain any leading 0's.
 
-
 public class Solution66 {
 
 	public int[] plusOne(int[] digits) {
-        int p = digits.length -1; // p is the index of the last digit
-        if(digits[p] < 9){ // if the last digit is less than 9, we can just increment it
-            digits[p] = ++digits[p]; // increment the last digit
-        }else{
-            do{
-                digits[p--] = 0; // if the last digit is 9, we need to set it to 0 and move to the previous digit
-                
-            }while(p>=0 && digits[p] == 9); // if the previous digit is 9, we need to set it to 0 and move to the previous digit
-            if(digits[0] != 0){ // if the first digit is not 0, we can just increment it
-                ++digits[p]; // increment the last digit
-            }else{
-              digits = new int[digits.length+1];  // if the first digit is 0, we need to add a new digit to the front
-                digits[0] = 1; // set the first digit to 1
-            }
-            
-        }
-        return digits; // return the new array
-    }
+		int p = digits.length - 1; // p is the index of the last digit
+		if (digits[p] < 9) { // if the last digit is less than 9, we can just increment it
+			digits[p] = ++digits[p]; // increment the last digit
+		} else {
+			do {
+				digits[p--] = 0; // if the last digit is 9, we need to set it to 0 and move to the previous digit
+
+			} while (p >= 0 && digits[p] == 9); // if the previous digit is 9, we need to set it to 0 and move to the
+												// previous digit
+			if (digits[0] != 0) { // if the first digit is not 0, we can just increment it
+				++digits[p]; // increment the last digit
+			} else {
+				digits = new int[digits.length + 1]; // if the first digit is 0, we need to add a new digit to the front
+				digits[0] = 1; // set the first digit to 1
+			}
+
+		}
+		return digits; // return the new array
+	}
 }
