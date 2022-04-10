204a472202dec23d1e9dab7688f7bcfa01ff7fdb
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-10 08:40:57 +0300

    change committed 1

--- src/main/java/com/task66/Solution66.java
+++ src/main/java/com/task66/Solution66.java
@@ -7,7 +7,6 @@ package com.task66;
 //where each digits[i] is the ith digit of the integer. 
 //The digits are ordered from most significant to least significant in left-to-right order. 
 //The large integer does not contain any leading 0's.
-
 //Increment the large integer by one and return the resulting array of digits.
 
 //Example 1:
