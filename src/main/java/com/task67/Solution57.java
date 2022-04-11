f833fe50e4ce58c8ad5da030d9e01887e3956f5a
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-11 07:07:12 +0300

    added solution

--- src/main/java/com/task67/Solution57.java
+++ src/main/java/com/task67/Solution57.java
@@ -24,5 +24,30 @@ package com.task67;
 //Each string does not contain leading zeros except for the zero itself.
 
 public class Solution57 {
-
-}
+	 public String addBinary(String a, String b) {
+	        StringBuilder sb = new StringBuilder(); // `StringBuilder` is faster than `String`
+	        int carry = 0, p1 = a.length() -1, p2 = b.length() -1; //  p1, p2 are the pointers of a, b
+	        while(p1 >= 0 && p2 >= 0){ //  while there are  still digits  to compare
+	    
+	        carry += a.charAt(p1--) - '0'; //   add the last digit of a to carry
+	       carry += b.charAt(p2--) - '0'; //  add the last digit of b to carry`
+	       sb.insert(0, (char)(carry %2 + '0'));  //  append the last digit of the sum to the front of the string
+	            carry >>=1;  //  divide the sum by 2
+	        } 
+	    while (p1 >= 0){ //  if there are still digits to compare in a
+	         carry += a.charAt(p1--) - '0'; //  add the last digit of a to carry
+	       
+	       sb.insert(0, (char)(carry %2 + '0')); //  append the last digit of the sum to the front of the string
+	            carry >>=1; //  divide the sum by 2
+	    }
+	        while(p2 >= 0){  
+	       carry += b.charAt(p2--) - '0'; //  add the last digit of b to carry
+	       sb.insert(0, (char)(carry %2 + '0')); //  append the last digit of the sum to the front of the string
+	            carry >>=1; //  divide the sum by 2
+	        }
+	        if(carry == 1){ //  if there is a carry left over
+	            sb.insert(0,'1'); //  append a 1 to the front of the string
+	        }
+	        return sb.toString(); //  return the string
+	    }
+	}
