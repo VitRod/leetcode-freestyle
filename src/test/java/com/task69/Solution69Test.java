3f92898e7ba88b0834a7805d531804dec6d26bf1
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-12 07:53:55 +0300

    formatting

--- src/test/java/com/task69/Solution69Test.java
+++ src/test/java/com/task69/Solution69Test.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class Solution69Test {
 
-	 Solution69 s = new Solution69();
- 
-	    @Test
-	    public void test1() {
-	        assertEquals(2, s.mySqrt(4));
-	    }
+	Solution69 s = new Solution69();
 
-	    @Test
-	    public void test2() {
-	        assertEquals(2, s.mySqrt(8));
-	    }
+	@Test
+	public void test1() {
+		assertEquals(2, s.mySqrt(4));
 	}
+
+	@Test
+	public void test2() {
+		assertEquals(2, s.mySqrt(8));
+	}
+}
