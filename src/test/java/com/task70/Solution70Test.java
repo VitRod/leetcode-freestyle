c752409c20f1f1fc57f18a28879d7642b083a720
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-13 06:37:01 +0300

    formatting

--- src/test/java/com/task70/Solution70Test.java
+++ src/test/java/com/task70/Solution70Test.java
@@ -6,17 +6,16 @@ import org.junit.jupiter.api.Test;
 
 class Solution70Test {
 
-	 Solution70 s = new Solution70();
+	Solution70 s = new Solution70();
 
-     
-	    @Test
-	    public void test1() {
-	        assertEquals(2, s.climbStairs(2));
-	    }
-
-	    @Test
-	    public void test2() {
-	        assertEquals(3, s.climbStairs(3));
-	    }
-
+	@Test
+	public void test1() {
+		assertEquals(2, s.climbStairs(2));
 	}
+
+	@Test
+	public void test2() {
+		assertEquals(3, s.climbStairs(3));
+	}
+
+}
