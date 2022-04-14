2bb3c4ddf1816b6ec248ac79887e77d0e45e99cf
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-14 07:12:45 +0300

    formatting

--- src/main/java/com/task83/Solution83.java
+++ src/main/java/com/task83/Solution83.java
@@ -6,19 +6,14 @@ package com.task83;
 //Given the head of a sorted linked list, delete all duplicates 
 //such that each element appears only once. Return the linked list sorted as well.
 
-
-
 //Example 1:
-
 
 //Input: head = [1,1,2]
 //Output: [1,2]
 //Example 2:
 
-
 //Input: head = [1,1,2,3,3]
 //Output: [1,2,3]
-
 
 //Constraints:
 
@@ -28,16 +23,17 @@ package com.task83;
 
 public class Solution83 {
 	public ListNode deleteDuplicates(ListNode head) {
-	      if(head == null || head.next == null) return head; // if there is only one node or no node, return the head
-	          ListNode curr = head; // create a new node to store the current node
-	        while(curr.next != null){ // while the next node is not null
-	            if(curr.next.val == curr.val){ // if the next node is the same as the current node
-	                curr.next = curr.next.next; // delete the next node
-	            }else{
-	                curr = curr.next; // if the next node is not the same as the current node, move to the next node
-	            }
-	        }
-	      
-	        return head; // return the head
-	    }
+		if (head == null || head.next == null)
+			return head; // if there is only one node or no node, return the head
+		ListNode curr = head; // create a new node to store the current node
+		while (curr.next != null) { // while the next node is not null
+			if (curr.next.val == curr.val) { // if the next node is the same as the current node
+				curr.next = curr.next.next; // delete the next node
+			} else {
+				curr = curr.next; // if the next node is not the same as the current node, move to the next node
+			}
+		}
+
+		return head; // return the head
 	}
+}
