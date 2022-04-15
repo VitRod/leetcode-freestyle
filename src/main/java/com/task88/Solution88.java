7653323b9ff02faab14d6503da05e370b92b2f4f
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-15 07:37:50 +0300

    added change commit 1

--- src/main/java/com/task88/Solution88.java
+++ src/main/java/com/task88/Solution88.java
@@ -14,7 +14,6 @@ package com.task88;
 //and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 //Example 1:
-
 //Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 //Output: [1,2,2,3,5,6]
 //Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
