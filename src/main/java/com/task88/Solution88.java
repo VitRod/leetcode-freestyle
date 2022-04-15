533665d9607d5c8747d3612e84b53a49273566f6
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-15 07:38:31 +0300

    added change commit 2

--- src/main/java/com/task88/Solution88.java
+++ src/main/java/com/task88/Solution88.java
@@ -18,6 +18,7 @@ package com.task88;
 //Output: [1,2,2,3,5,6]
 //Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 //The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
 //Example 2:
 
 //Input: nums1 = [1], m = 1, nums2 = [], n = 0
