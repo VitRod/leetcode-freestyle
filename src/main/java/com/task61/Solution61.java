dd710f3b188ad4fa657d4127620e2da682097e86
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-16 07:27:30 +0300

    added change commit 1

--- src/main/java/com/task61/Solution61.java
+++ src/main/java/com/task61/Solution61.java
@@ -6,7 +6,6 @@ package com.task61;
 //Given the head of a linked list, rotate the list to the right by k places.
 
 //Example 1:
-
 //Input: head = [1,2,3,4,5], k = 2
 //Output: [4,5,1,2,3]
 //Example 2:
