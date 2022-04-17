d1b4610fbee31cd6b0ad63d371572b5ebf0e6bed
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-17 07:42:21 +0300

    formatting

--- src/test/java/com/task62/Solution62Test.java
+++ src/test/java/com/task62/Solution62Test.java
@@ -8,13 +8,13 @@ class Solution62Test {
 
 	Solution62 s = new Solution62();
 
-    @Test
-    public void test1() {
-        assertEquals(28, s.uniquePaths(3, 7));
-    }
+	@Test
+	public void test1() {
+		assertEquals(28, s.uniquePaths(3, 7));
+	}
 
-    @Test
-    public void test2() {
-        assertEquals(3, s.uniquePaths(3, 2));
-    }
+	@Test
+	public void test2() {
+		assertEquals(3, s.uniquePaths(3, 2));
+	}
 }
