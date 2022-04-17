c0075ef8c61d808d8db7dcb64984a3d870da38a4
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-18 02:33:49 +0300

    formatting

--- src/test/java/com/task63/Solution63Test.java
+++ src/test/java/com/task63/Solution63Test.java
@@ -8,22 +8,15 @@ class Solution63Test {
 
 	Solution63 s = new Solution63();
 
-    @Test
-    public void test1() {
-        int[][] grid = new int[][] {
-            {0, 0, 0},
-            {0, 1, 0},
-            {0, 0, 0}
-        };
-        assertEquals(2, s.uniquePathsWithObstacles(grid));
-    }
+	@Test
+	public void test1() {
+		int[][] grid = new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+		assertEquals(2, s.uniquePathsWithObstacles(grid));
+	}
 
-    @Test
-    public void test2() {
-        int[][] grid = new int[][] {
-            {0, 1},
-            {0, 0}
-        };
-        assertEquals(1, s.uniquePathsWithObstacles(grid));
-    }
+	@Test
+	public void test2() {
+		int[][] grid = new int[][] { { 0, 1 }, { 0, 0 } };
+		assertEquals(1, s.uniquePathsWithObstacles(grid));
+	}
 }
