815d103d9d5a4c1b1a4e893ec8f11b2b8f535714
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-18 02:33:17 +0300

    added tests

--- src/test/java/com/task63/Solution63Test.java
+++ src/test/java/com/task63/Solution63Test.java
@@ -6,9 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class Solution63Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution63 s = new Solution63();
 
+    @Test
+    public void test1() {
+        int[][] grid = new int[][] {
+            {0, 0, 0},
+            {0, 1, 0},
+            {0, 0, 0}
+        };
+        assertEquals(2, s.uniquePathsWithObstacles(grid));
+    }
+
+    @Test
+    public void test2() {
+        int[][] grid = new int[][] {
+            {0, 1},
+            {0, 0}
+        };
+        assertEquals(1, s.uniquePathsWithObstacles(grid));
+    }
 }
