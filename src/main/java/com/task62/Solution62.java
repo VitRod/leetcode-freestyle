8b54f00f97877f4a09ecc961b04e94d46f4e2ac1
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-17 07:38:37 +0300

    formatting

--- src/main/java/com/task62/Solution62.java
+++ src/main/java/com/task62/Solution62.java
@@ -1,8 +1,8 @@
 package com.task62;
 
 //62. Unique Paths
-//Medium
 
+//Medium
 
 //There is a robot on an m x n grid. The robot is initially located
 //at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner 
@@ -13,10 +13,7 @@ package com.task62;
 
 //The test cases are generated so that the answer will be less than or equal to 2 * 109.
 
-
-
 //Example 1:
-
 
 //Input: m = 3, n = 7
 //Output: 28
@@ -29,25 +26,23 @@ package com.task62;
 //2. Down -> Down -> Right
 //3. Down -> Right -> Down
 
-
 //Constraints:
 
 //1 <= m, n <= 100
 
-
 public class Solution62 {
-	
-	    public int uniquePaths(int m, int n) {
-	        int [] [] dp = new int [m][n]; // dp[i][j] = number of paths to reach (i,j)
-	        for(int i = 0; i < m; i++) { // initialize
-	            for(int j = 0; j < n; j++) { // initialize
-	                if(i == 0 || j == 0) { // if (i,j) is the top-left corner
-	                    dp[i][j] = 1; // then there is only one path to reach (i,j)
-	                } else {
-	                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // otherwise, there are two paths to reach (i,j)
-	                }
-	            }
-	        }
-	        return dp[m - 1][n - 1]; // return the number of paths to reach (m - 1, n - 1)
-	    }
+
+	public int uniquePaths(int m, int n) {
+		int[][] dp = new int[m][n]; // dp[i][j] = number of paths to reach (i,j)
+		for (int i = 0; i < m; i++) { // initialize
+			for (int j = 0; j < n; j++) { // initialize
+				if (i == 0 || j == 0) { // if (i,j) is the top-left corner
+					dp[i][j] = 1; // then there is only one path to reach (i,j)
+				} else {
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // otherwise, there are two paths to reach (i,j)
+				}
+			}
+		}
+		return dp[m - 1][n - 1]; // return the number of paths to reach (m - 1, n - 1)
 	}
+}
