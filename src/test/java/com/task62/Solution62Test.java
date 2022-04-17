cbc65c20e6617c4f393b636d553232abd194e800
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-17 07:41:28 +0300

    added tests

--- src/test/java/com/task62/Solution62Test.java
+++ src/test/java/com/task62/Solution62Test.java
@@ -6,9 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class Solution62Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution62 s = new Solution62();
 
+    @Test
+    public void test1() {
+        assertEquals(28, s.uniquePaths(3, 7));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(3, s.uniquePaths(3, 2));
+    }
 }
