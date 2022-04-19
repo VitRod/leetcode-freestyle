fa957d81062a8c2887ec034b7271e4dc0a21cad0
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-19 04:02:44 +0300

    generated test stub

---
+++ src/test/java/com/task64/Solution64Test.java
@@ -0,0 +1,14 @@
+package com.task64;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution64Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
