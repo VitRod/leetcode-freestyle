7ef8e43e11230af6b55afff2dc0877033db71586
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-22 04:39:50 +0300

    added tests

--- src/test/java/com/task74/Solution74Test.java
+++ src/test/java/com/task74/Solution74Test.java
@@ -6,9 +6,31 @@ import org.junit.jupiter.api.Test;
 
 class Solution74Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution74 s = new Solution74();
 
+    @Test
+    public void test1() {
+        int[][] matrix = new int[][] {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        int target = 3;
+        boolean expected = true;
+        boolean actual = s.searchMatrix(matrix, target);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        int[][] matrix = new int[][] {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 60}
+        };
+        int target = 13;
+        boolean expected = false;
+        boolean actual = s.searchMatrix(matrix, target);
+        assertEquals(expected, actual);
+    }
 }
