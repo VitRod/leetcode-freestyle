c6f013fad24097f7c13422acda39ef9fbdbcef54
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-23 04:50:04 +0300

    added tests

---
+++ src/test/java/com/task75/Solution75Test.java
@@ -0,0 +1,32 @@
+package com.task75;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Solution75Test {
+
+	Solution75 s = new Solution75();
+
+    @Test
+    public void test1() {
+        int[] nums = {2,0,2,1,1,0};
+        
+        s.sortColors(nums);
+        assertEquals(0, nums[0]);
+        assertEquals(0, nums[1]);
+        assertEquals(1, nums[2]);
+        assertEquals(1, nums[3]);
+        assertEquals(2, nums[4]);
+        assertEquals(2, nums[5]);
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {2,0,1};
+        s.sortColors(nums);
+        assertEquals(0, nums[0]);
+        assertEquals(1, nums[1]);
+        assertEquals(2, nums[2]);
+    }
+}
