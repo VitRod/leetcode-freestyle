df3efe685ff0a3f275f0da02988872660098edf9
Vitaliy Rodyna <ukrdao@gmail.com>
2022-04-23 04:51:09 +0300

    formatting

--- src/test/java/com/task75/Solution75Test.java
+++ src/test/java/com/task75/Solution75Test.java
@@ -8,25 +8,25 @@ class Solution75Test {
 
 	Solution75 s = new Solution75();
 
-    @Test
-    public void test1() {
-        int[] nums = {2,0,2,1,1,0};
-        
-        s.sortColors(nums);
-        assertEquals(0, nums[0]);
-        assertEquals(0, nums[1]);
-        assertEquals(1, nums[2]);
-        assertEquals(1, nums[3]);
-        assertEquals(2, nums[4]);
-        assertEquals(2, nums[5]);
-    }
+	@Test
+	public void test1() {
+		int[] nums = { 2, 0, 2, 1, 1, 0 };
 
-    @Test
-    public void test2() {
-        int[] nums = {2,0,1};
-        s.sortColors(nums);
-        assertEquals(0, nums[0]);
-        assertEquals(1, nums[1]);
-        assertEquals(2, nums[2]);
-    }
+		s.sortColors(nums);
+		assertEquals(0, nums[0]);
+		assertEquals(0, nums[1]);
+		assertEquals(1, nums[2]);
+		assertEquals(1, nums[3]);
+		assertEquals(2, nums[4]);
+		assertEquals(2, nums[5]);
+	}
+
+	@Test
+	public void test2() {
+		int[] nums = { 2, 0, 1 };
+		s.sortColors(nums);
+		assertEquals(0, nums[0]);
+		assertEquals(1, nums[1]);
+		assertEquals(2, nums[2]);
+	}
 }
