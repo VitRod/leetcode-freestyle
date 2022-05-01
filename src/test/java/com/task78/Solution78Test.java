6f51a462aed9045221b1c888ae222d2449b71449
Vitaliy Rodyna <ukrdao@gmail.com>
2022-05-01 05:32:41 +0300

    formatting

--- src/test/java/com/task78/Solution78Test.java
+++ src/test/java/com/task78/Solution78Test.java
@@ -7,21 +7,21 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class Solution78Test {
-   
-     Solution78 s = new Solution78();
 
-       @Test
-         public void test1() {
-            int[] nums = {1,2,3};
-            List<List<Integer>> result = s.subsets(nums);
-            assertEquals(8, result.size());
-         }
+	Solution78 s = new Solution78();
 
-         @Test
-         public void test2() {
-            int[] nums = {0};
-            List<List<Integer>> result = s.subsets(nums);
-            assertEquals(2, result.size());
-         }
+	@Test
+	public void test1() {
+		int[] nums = { 1, 2, 3 };
+		List<List<Integer>> result = s.subsets(nums);
+		assertEquals(8, result.size());
+	}
 
-      }
+	@Test
+	public void test2() {
+		int[] nums = { 0 };
+		List<List<Integer>> result = s.subsets(nums);
+		assertEquals(2, result.size());
+	}
+
+}
