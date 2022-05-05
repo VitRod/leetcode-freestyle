dd329fa37cfa443fe271e329e278f5e36be812ff
Vitaliy Rodyna <ukrdao@gmail.com>
2022-05-05 06:02:40 +0300

    formatting

--- src/test/java/com/task56/Solution56Test.java
+++ src/test/java/com/task56/Solution56Test.java
@@ -10,24 +10,24 @@ class Solution56Test {
 
 	Solution56 s = new Solution56();
 
-	   @Test
-	   public void test1() {
-	      int[][] intervals = new int[][] {{1,3}, {2,6}, {8,10}, {15,18}};
-	      int[][] expected = new int[][] {{1,6}, {8,10}, {15,18}};
-	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
-	   }
-
-	   @Test
-	   public void test2() {
-	      int[][] intervals = new int[][] {{1,4}, {4,5}};
-	      int[][] expected = new int[][] {{1,5}};
-	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
-	   }
-
-	   @Test
-	   public void test3() {
-	      int[][] intervals = new int[][] {{1,5}, {2,3}};
-	      int[][] expected = new int[][] {{1,5}};
-	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
-	   }
+	@Test
+	public void test1() {
+		int[][] intervals = new int[][] { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
+		int[][] expected = new int[][] { { 1, 6 }, { 8, 10 }, { 15, 18 } };
+		assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
 	}
+
+	@Test
+	public void test2() {
+		int[][] intervals = new int[][] { { 1, 4 }, { 4, 5 } };
+		int[][] expected = new int[][] { { 1, 5 } };
+		assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
+	}
+
+	@Test
+	public void test3() {
+		int[][] intervals = new int[][] { { 1, 5 }, { 2, 3 } };
+		int[][] expected = new int[][] { { 1, 5 } };
+		assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
+	}
+}
