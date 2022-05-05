0778516fc17fed2c78685755bfa7f7be850b76c6
Vitaliy Rodyna <ukrdao@gmail.com>
2022-05-05 06:02:08 +0300

    added test

--- src/test/java/com/task56/Solution56Test.java
+++ src/test/java/com/task56/Solution56Test.java
@@ -2,13 +2,32 @@ package com.task56;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Solution56Test {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Solution56 s = new Solution56();
 
-}
+	   @Test
+	   public void test1() {
+	      int[][] intervals = new int[][] {{1,3}, {2,6}, {8,10}, {15,18}};
+	      int[][] expected = new int[][] {{1,6}, {8,10}, {15,18}};
+	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
+	   }
+
+	   @Test
+	   public void test2() {
+	      int[][] intervals = new int[][] {{1,4}, {4,5}};
+	      int[][] expected = new int[][] {{1,5}};
+	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
+	   }
+
+	   @Test
+	   public void test3() {
+	      int[][] intervals = new int[][] {{1,5}, {2,3}};
+	      int[][] expected = new int[][] {{1,5}};
+	      assertEquals(Arrays.deepToString(expected), Arrays.deepToString(s.merge(intervals)));
+	   }
+	}
