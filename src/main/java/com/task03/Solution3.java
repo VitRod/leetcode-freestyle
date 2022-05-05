1e548f704fc19d552afdb36f4c2d32576606ad3f
VitRod <ukrdao@gmail.com>
2022-05-05 06:03:48 +0300

    Update Solution3.java

--- src/main/java/com/task03/Solution3.java
+++ src/main/java/com/task03/Solution3.java
@@ -23,7 +23,7 @@ package com.task03;
 //Output: 3
 //Explanation: The answer is "wke", with the length of 3.
 //Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
+    
 
 //Constraints:
 
