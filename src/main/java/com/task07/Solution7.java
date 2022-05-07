f1c4eff9b2b9774e5b26c5b8d0629d3bf78f73ec
VitRod <ukrdao@gmail.com>
2022-05-07 16:54:36 +0300

    Update Solution7.java

--- src/main/java/com/task07/Solution7.java
+++ src/main/java/com/task07/Solution7.java
@@ -2,7 +2,7 @@ package com.task07;
 
 //7. Reverse Integer
 //Medium
-
+    
 //Given a signed 32-bit integer x, return x with its digits reversed. 
 //If reversing x causes the value to go outside the signed 
 //32-bit integer range [-231, 231 - 1], then return 0.
