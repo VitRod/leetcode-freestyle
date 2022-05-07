16262c965b49d059c359323f40adca38a10221e9
VitRod <ukrdao@gmail.com>
2022-05-07 16:58:14 +0300

    Update Solution8.java

--- src/main/java/com/task08/Solution8.java
+++ src/main/java/com/task08/Solution8.java
@@ -2,7 +2,7 @@ package com.task08;
 
 //8. String to Integer (atoi)
 //Medium
-
+    
 //Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
 
 //The algorithm for myAtoi(string s) is as follows:
