a5ad34c2e271bb43f91a54d4b4ae1893ea9e9a43
VitRod <ukrdao@gmail.com>
2022-05-07 17:00:33 +0300

    Update Solution5.java

--- src/main/java/com/task05/Solution5.java
+++ src/main/java/com/task05/Solution5.java
@@ -6,7 +6,7 @@ package com.task05;
 //Given a string s, return the longest palindromic substring in s.
         
 //Example 1:
-        
+           
 //Input: s = "babad"
 //Output: "bab"
 //Explanation: "aba" is also a valid answer.
