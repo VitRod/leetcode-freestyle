662082ee55e88b374a8dab74405b53615eb8a391
VitRod <ukrdao@gmail.com>
2022-05-07 07:55:33 +0300

    Update Solution6.java

--- src/main/java/com/task06/Solution6.java
+++ src/main/java/com/task06/Solution6.java
@@ -2,7 +2,7 @@ package com.task06;
     
 //6. Zigzag Conversion
 //Medium
-    
+        
 //The string "PAYPALISHIRING" is written in a zigzag pattern on a given 
 //number of rows like this: (you may want to display this pattern 
 //in a fixed font for better legibility)
