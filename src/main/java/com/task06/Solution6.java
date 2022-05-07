b66f0c2bbe99dadd2c7d5796f59b855f69bc3a3b
VitRod <ukrdao@gmail.com>
2022-05-07 07:55:03 +0300

    Update Solution6.java

--- src/main/java/com/task06/Solution6.java
+++ src/main/java/com/task06/Solution6.java
@@ -11,7 +11,7 @@ package com.task06;
 //A P L S I I G
 //Y   I   R
 //And then read line by line: "PAHNAPLSIIGYIR"
-
+     
 //Write the code that will take a string and make this conversion given a number of rows:
     
 //string convert(string s, int numRows);
