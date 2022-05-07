aba944663d65cc96f6bc3ed9591b9726f3e1e84a
VitRod <ukrdao@gmail.com>
2022-05-07 17:11:15 +0300

    Update Solution10.java

--- src/main/java/com/task10/Solution10.java
+++ src/main/java/com/task10/Solution10.java
@@ -8,7 +8,7 @@ package com.task10;
 //'.' Matches any single character.​​​​
 //'*' Matches zero or more of the preceding element.
 //The matching should cover the entire input string (not partial).
-
+   
 //Example 1:
 
 //Input: s = "aa", p = "a"
