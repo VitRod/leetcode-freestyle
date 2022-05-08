2cac6c5a2177e6e7924d7c1908c37ff76c73b689
VitRod <ukrdao@gmail.com>
2022-05-08 11:49:50 +0300

    Update Solution24.java

--- src/main/java/com/task24/Solution24.java
+++ src/main/java/com/task24/Solution24.java
@@ -6,7 +6,7 @@ package com.task24;
 //You must solve the problem without modifying the values in the list's nodes 
 //(i.e., only nodes themselves may be changed.)     
 
-//Example 1:
+//Example 1:  
 
 //Input: head = [1,2,3,4]
 //Output: [2,1,4,3]
