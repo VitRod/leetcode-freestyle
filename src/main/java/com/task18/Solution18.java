1f97eb8d294a1b2ee6944463983b9434e18d97fb
VitRod <ukrdao@gmail.com>
2022-05-08 11:12:02 +0300

    Update Solution18.java

--- src/main/java/com/task18/Solution18.java
+++ src/main/java/com/task18/Solution18.java
@@ -2,7 +2,7 @@ package com.task18;
      
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;            
+import java.util.List;                
        
 // 18. 4Sum
 // Medium        
