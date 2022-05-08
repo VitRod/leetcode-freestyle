a0bfbf9c1d8d20179ee02c109cea3b9b0f3cfb1b
VitRod <ukrdao@gmail.com>
2022-05-08 09:16:29 +0300

    Update Solution14.java

--- src/main/java/com/task14/Solution14.java
+++ src/main/java/com/task14/Solution14.java
@@ -2,7 +2,7 @@ package com.task14;
       
 //14. Longest Common Prefix
 //Easy
-         
+            
 //Write a function to find the longest common prefix string amongst an array of strings.
      
 //If there is no common prefix, return an empty string "".
