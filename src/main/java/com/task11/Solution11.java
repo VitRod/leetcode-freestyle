2dac0db54f40948bd60b7b59c0d2cb192b048cdf
VitRod <ukrdao@gmail.com>
2022-05-08 06:05:33 +0300

    Update Solution11.java

--- src/main/java/com/task11/Solution11.java
+++ src/main/java/com/task11/Solution11.java
@@ -2,7 +2,7 @@ package com.task11;
 
 //11. Container With Most Water
 //Medium
-    
+        
 //You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
      
 //Find two lines that together with the x-axis form a container, such that the container contains the most water.
