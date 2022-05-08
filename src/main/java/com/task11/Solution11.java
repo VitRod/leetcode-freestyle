0c40a9397544585a9533ff5b535ce253228a35e1
VitRod <ukrdao@gmail.com>
2022-05-08 06:06:04 +0300

    Update Solution11.java

--- src/main/java/com/task11/Solution11.java
+++ src/main/java/com/task11/Solution11.java
@@ -17,7 +17,7 @@ package com.task11;
 //Output: 49
 //Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
 //Example 2:
-
+      
 //Input: height = [1,1]
 //Output: 1
 
