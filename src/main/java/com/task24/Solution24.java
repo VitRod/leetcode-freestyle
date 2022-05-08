f69e0b6fc2fc5cb4bf7f5deddac670d6b8c7ff2d
VitRod <ukrdao@gmail.com>
2022-05-08 11:49:31 +0300

    Update Solution24.java

--- src/main/java/com/task24/Solution24.java
+++ src/main/java/com/task24/Solution24.java
@@ -1,6 +1,6 @@
 package com.task24;
 
-//24. Swap Nodes in Pairs
+//24. Swap Nodes in Pairs   
 
 //Given a linked list, swap every two adjacent nodes and return its head. 
 //You must solve the problem without modifying the values in the list's nodes 
