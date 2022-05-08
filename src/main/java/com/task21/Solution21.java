5d38ef170f7bf8cc514a7a1ac50d916f959218c9
VitRod <ukrdao@gmail.com>
2022-05-08 11:21:22 +0300

    Update Solution21.java

--- src/main/java/com/task21/Solution21.java
+++ src/main/java/com/task21/Solution21.java
@@ -13,7 +13,7 @@ package com.task21;
 //Example 1:   
    
 //Input: list1 = [1,2,4], list2 = [1,3,4]
-//Output: [1,1,2,3,4,4]
+//Output: [1,1,2,3,4,4]   
 //Example 2:
 
 //Input: list1 = [], list2 = []
