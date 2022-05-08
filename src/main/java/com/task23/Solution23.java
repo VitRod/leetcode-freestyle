687065094174f298a908fc1603b2806d66c2ad16
VitRod <ukrdao@gmail.com>
2022-05-08 11:46:17 +0300

    Update Solution23.java

--- src/main/java/com/task23/Solution23.java
+++ src/main/java/com/task23/Solution23.java
@@ -9,7 +9,7 @@ package com.task23;
 
 //Example 1:   
   
-//Input: lists = [[1,4,5],[1,3,4],[2,6]]
+//Input: lists = [[1,4,5],[1,3,4],[2,6]]   
 //Output: [1,1,2,3,4,4,5,6]
 //Explanation: The linked-lists are:
 //[
