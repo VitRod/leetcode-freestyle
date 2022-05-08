13f5b0b9efcb2f3c447282449aac04085cc90bcf
VitRod <ukrdao@gmail.com>
2022-05-08 09:17:53 +0300

    Update Solution14.java

--- src/main/java/com/task14/Solution14.java
+++ src/main/java/com/task14/Solution14.java
@@ -18,7 +18,7 @@ package com.task14;
 //Explanation: There is no common prefix among the input strings.
                     
 //Constraints:
-      
+           
 //1 <= strs.length <= 200
 //0 <= strs[i].length <= 200
 //strs[i] consists of only lower-case English letters.
