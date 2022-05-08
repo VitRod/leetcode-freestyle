13a88d32342b71fc31a626949cc9fe504fd18e04
VitRod <ukrdao@gmail.com>
2022-05-08 11:13:59 +0300

    Update Solution19.java

--- src/main/java/com/task19/Solution19.java
+++ src/main/java/com/task19/Solution19.java
@@ -2,7 +2,7 @@ package com.task19;
 
 //19. Remove Nth Node From End of List
 //Medium
-
+   
 //Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 //Example 1:    
