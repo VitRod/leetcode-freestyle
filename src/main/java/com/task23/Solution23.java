9ff1be19a9f7182d70bcf58e3626923f158f21a4
VitRod <ukrdao@gmail.com>
2022-05-08 11:46:24 +0300

    Update Solution23.java

--- src/main/java/com/task23/Solution23.java
+++ src/main/java/com/task23/Solution23.java
@@ -5,7 +5,7 @@ package com.task23;
 
 //You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
-//Merge all the linked-lists into one sorted linked-list and return it.
+//Merge all the linked-lists into one sorted linked-list and return it.   
 
 //Example 1:   
   
