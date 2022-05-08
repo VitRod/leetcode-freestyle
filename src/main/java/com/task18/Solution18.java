0ea30b62545854c90d33985e8df2f1d290ab6c81
VitRod <ukrdao@gmail.com>
2022-05-08 11:12:09 +0300

    Update Solution18.java

--- src/main/java/com/task18/Solution18.java
+++ src/main/java/com/task18/Solution18.java
@@ -15,7 +15,7 @@ import java.util.List;
 // nums[a] + nums[b] + nums[c] + nums[d] == target
 // You may return the answer in any order.
 
-// Example 1:
+// Example 1:     
 
 // Input: nums = [1,0,-1,0,-2,2], target = 0
 // Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
