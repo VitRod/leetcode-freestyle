331e9b7302bb62a7f7953e42250d38c145cf0af7
VitRod <ukrdao@gmail.com>
2022-05-08 09:17:01 +0300

    Update Solution14.java

--- src/main/java/com/task14/Solution14.java
+++ src/main/java/com/task14/Solution14.java
@@ -12,7 +12,7 @@ package com.task14;
 //Input: strs = ["flower","flow","flight"]
 //Output: "fl"
 //Example 2:
-
+      
 //Input: strs = ["dog","racecar","car"]
 //Output: ""
 //Explanation: There is no common prefix among the input strings.
