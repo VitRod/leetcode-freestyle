6023ce845a5b3d38a03c3b23e148f3202982d50f
VitRod <ukrdao@gmail.com>
2022-05-08 11:46:10 +0300

    Update Solution23.java

--- src/main/java/com/task23/Solution23.java
+++ src/main/java/com/task23/Solution23.java
@@ -8,7 +8,7 @@ package com.task23;
 //Merge all the linked-lists into one sorted linked-list and return it.
 
 //Example 1:   
-
+  
 //Input: lists = [[1,4,5],[1,3,4],[2,6]]
 //Output: [1,1,2,3,4,4,5,6]
 //Explanation: The linked-lists are:
