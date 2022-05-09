9ddb1453d88d5526095ed784f134fd5f051a71d4
VitRod <ukrdao@gmail.com>
2022-05-09 09:17:14 +0300

    Update Solution30.java

--- src/main/java/com/task30/Solution30.java
+++ src/main/java/com/task30/Solution30.java
@@ -11,7 +11,7 @@ import java.util.Map;
 // You are given a string s and an array of strings words of the same length. 
 // Return all starting indices of substring(s) in s that is a concatenation of each word 
 // in words exactly once, in any order, and without any intervening characters.
-    
+       
 // You can return the answer in any order.
 
 // Example 1:
