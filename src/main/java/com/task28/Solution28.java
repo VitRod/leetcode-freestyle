5c1203c2195aa658cb55e6a220956f4b80285cd5
VitRod <ukrdao@gmail.com>
2022-05-09 09:10:26 +0300

    Update Solution28.java

--- src/main/java/com/task28/Solution28.java
+++ src/main/java/com/task28/Solution28.java
@@ -12,7 +12,7 @@ package com.task28;
 //What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 //For the purpose of this problem, we will return 0 when needle is an empty string. 
-//This is consistent to C's strstr() and Java's indexOf().
+//This is consistent to C's strstr() and Java's indexOf().    
 
 //Example 1:
 
