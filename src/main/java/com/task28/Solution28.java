fa1b5c326b4d1e01a4e9e20deb5a511964ffa5ae
VitRod <ukrdao@gmail.com>
2022-05-09 09:10:05 +0300

    Update Solution28.java

--- src/main/java/com/task28/Solution28.java
+++ src/main/java/com/task28/Solution28.java
@@ -4,7 +4,7 @@ package com.task28;
 //Easy
 
 //Implement strStr().
-
+     
 //Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 //Clarification:
