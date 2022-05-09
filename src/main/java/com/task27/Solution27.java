9f55be2606f2ca04399d465c3d840ed734370444
VitRod <ukrdao@gmail.com>
2022-05-09 09:08:08 +0300

    Update Solution27.java

--- src/main/java/com/task27/Solution27.java
+++ src/main/java/com/task27/Solution27.java
@@ -11,7 +11,7 @@ package com.task27;
 //if there are k elements after removing the duplicates, then the first k elements 
 //of nums should hold the final result. It does not matter what you leave beyond the first k elements.
    
-//Return k after placing the final result in the first k slots of nums.   
+//Return k after placing the final result in the first k slots of nums.      
    
 //Do not allocate extra space for another array. You must do this 
 //by modifying the input array in-place with O(1) extra memory.
