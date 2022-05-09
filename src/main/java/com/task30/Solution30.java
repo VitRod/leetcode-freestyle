d7a38b07d77489abac9837774c10f371de66e778
VitRod <ukrdao@gmail.com>
2022-05-09 09:16:48 +0300

    Update Solution30.java

--- src/main/java/com/task30/Solution30.java
+++ src/main/java/com/task30/Solution30.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;    
 
 // 30. Substring with Concatenation of All Words      
-// Hard    
+// Hard        
       
 // You are given a string s and an array of strings words of the same length. 
 // Return all starting indices of substring(s) in s that is a concatenation of each word 
