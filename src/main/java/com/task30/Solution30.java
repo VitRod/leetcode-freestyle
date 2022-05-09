3495ce73f9e048b1fa14d8fccccd20a7d6c2afd7
VitRod <ukrdao@gmail.com>
2022-05-09 09:17:01 +0300

    Update Solution30.java

--- src/main/java/com/task30/Solution30.java
+++ src/main/java/com/task30/Solution30.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;   
 import java.util.Map;    
-
+    
 // 30. Substring with Concatenation of All Words      
 // Hard        
           
