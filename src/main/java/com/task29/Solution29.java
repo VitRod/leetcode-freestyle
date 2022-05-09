f595621af773939ea7e20a9e1af1f043b3243346
VitRod <ukrdao@gmail.com>
2022-05-09 09:14:09 +0300

    Update Solution29.java

--- src/main/java/com/task29/Solution29.java
+++ src/main/java/com/task29/Solution29.java
@@ -5,7 +5,7 @@ package com.task29;
 
 //Given two integers dividend and divisor, divide two integers without using multiplication,
 //division, and mod operator.
-
+   
 //The integer division should truncate toward zero, which means losing its fractional part.
 //For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
 
