7ef97dac3868686efe05335a8595a559bf4be648
VitRod <ukrdao@gmail.com>
2022-05-09 09:18:09 +0300

    Update Solution31.java

--- src/main/java/com/task31/Solution31.java
+++ src/main/java/com/task31/Solution31.java
@@ -2,7 +2,7 @@ package com.task31;
     
 //31. Next Permutation
 //Medium
-
+   
 //A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
 
 //For example, for arr = [1,2,3], the following are considered permutations of arr: 
