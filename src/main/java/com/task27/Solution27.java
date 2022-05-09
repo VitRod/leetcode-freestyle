56097edfb184ba39ca1225fe0c14d1ac875fc6b3
VitRod <ukrdao@gmail.com>
2022-05-09 09:08:44 +0300

    Update Solution27.java

--- src/main/java/com/task27/Solution27.java
+++ src/main/java/com/task27/Solution27.java
@@ -5,7 +5,7 @@ package com.task27;
            
 //Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
 //The relative order of the elements may be changed.           
-    
+        
 //Since it is impossible to change the length of the array in some languages, 
 //you must instead have the result be placed in the first part of the array nums. More formally, 
 //if there are k elements after removing the duplicates, then the first k elements 
