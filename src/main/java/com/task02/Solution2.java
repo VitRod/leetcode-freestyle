fbe77feb49fefa03f2f4fbaab2cc5c6e3c28da35
VitRod <ukrdao@gmail.com>
2022-05-10 08:24:28 +0300

    Update Solution2.java
    formatting

--- src/main/java/com/task02/Solution2.java
+++ src/main/java/com/task02/Solution2.java
@@ -2,7 +2,7 @@ package com.task02;
                   
 //2. Add Two Numbers
 //Medium
-               
+                  
 //You are given two non-empty linked lists representing two non-negative integers. 
 //The digits are stored in reverse order, and each of their nodes contains a single digit.
 //Add the two numbers and return the sum as a linked list.
