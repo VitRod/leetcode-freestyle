be81213c0ca03aa910c8f4c0376b21066ce0c581
VitRod <ukrdao@gmail.com>
2022-05-13 07:20:23 +0300

    Update Solution6.java

--- src/main/java/com/task06/Solution6.java
+++ src/main/java/com/task06/Solution6.java
@@ -6,7 +6,7 @@ package com.task06;
 //The string "PAYPALISHIRING" is written in a zigzag pattern on a given 
 //number of rows like this: (you may want to display this pattern 
 //in a fixed font for better legibility)
-      
+         
 //P   A   H   N
 //A P L S I I G
 //Y   I   R
