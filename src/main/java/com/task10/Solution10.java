0a3467d5c1d49f130ce52bbf8c280716be8ee6c8
VitRod <ukrdao@gmail.com>
2022-05-16 06:20:20 +0300

    commit solution

--- src/main/java/com/task10/Solution10.java
+++ src/main/java/com/task10/Solution10.java
@@ -5,7 +5,7 @@ package com.task10;
              
 //Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
       
-//'.' Matches any single character.​​​​
+//'.' Matches any single character.​​​​   
 //'*' Matches zero or more of the preceding element.
 //The matching should cover the entire input string (not partial).
    
