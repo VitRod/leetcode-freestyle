f4ab1f2455819b77f6a0e53336e03040c1c836ac
VitRod <ukrdao@gmail.com>
2022-05-18 07:20:07 +0300

    commit solution

--- src/main/java/com/task14/Solution14.java
+++ src/main/java/com/task14/Solution14.java
@@ -6,7 +6,7 @@ package com.task14;
 //Write a function to find the longest common prefix string amongst an array of strings.
      
 //If there is no common prefix, return an empty string "".
-    
+       
 //Example 1:         
         
 //Input: strs = ["flower","flow","flight"]
