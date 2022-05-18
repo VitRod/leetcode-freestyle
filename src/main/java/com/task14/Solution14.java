33d4bea399087610da2613cc4f992fda8119c075
VitRod <ukrdao@gmail.com>
2022-05-18 07:19:32 +0300

    commit solution

--- src/main/java/com/task14/Solution14.java
+++ src/main/java/com/task14/Solution14.java
@@ -8,7 +8,7 @@ package com.task14;
 //If there is no common prefix, return an empty string "".
     
 //Example 1:   
-     
+        
 //Input: strs = ["flower","flow","flight"]
 //Output: "fl"
 //Example 2:
