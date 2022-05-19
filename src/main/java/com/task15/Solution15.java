c7ab8656955b2ce28cbec8caf0b4fb9dc8154972
VitRod <ukrdao@gmail.com>
2022-05-19 06:25:55 +0300

    commit solution

--- src/main/java/com/task15/Solution15.java
+++ src/main/java/com/task15/Solution15.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
                   
 // 15. 3Sum  
-// Medium        
+// Medium           
                     
 // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such 
 // that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
