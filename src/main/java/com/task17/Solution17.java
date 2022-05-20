d9308583dc53172d5e7a083d2af72e1d3a6585f6
VitRod <ukrdao@gmail.com>
2022-05-20 08:34:08 +0300

    added files

--- src/main/java/com/task17/Solution17.java
+++ src/main/java/com/task17/Solution17.java
@@ -6,7 +6,7 @@ import java.util.List;
 // 17. Letter Combinations of a Phone Number   
 // Medium          
               
-// Given a string containing digits from 2-9 inclusive, 
+// Given a string containing digits from 2-9 inclusive,    
 // return all possible letter combinations that the number could represent. Return the answer in any order.
 
 // A mapping of digit to letters (just like on the telephone buttons) is given below. 
