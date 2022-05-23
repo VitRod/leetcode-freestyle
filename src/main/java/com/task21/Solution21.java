bac9fb934268bf17f01b3fbba4bb5733e7a3f5e2
VitRod <ukrdao@gmail.com>
2022-05-23 08:09:32 +0300

    added files

--- src/main/java/com/task21/Solution21.java
+++ src/main/java/com/task21/Solution21.java
@@ -11,7 +11,7 @@ package com.task21;
 //Return the head of the merged linked list.   
    
 //Example 1:   
-   
+       
 //Input: list1 = [1,2,4], list2 = [1,3,4]
 //Output: [1,1,2,3,4,4]   
 //Example 2:
