acbd5209fceb6ce878de3cc388f66962bf74028b
VitRod <ukrdao@gmail.com>
2022-05-23 08:09:23 +0300

    added files

--- src/main/java/com/task21/Solution21.java
+++ src/main/java/com/task21/Solution21.java
@@ -4,7 +4,7 @@ package com.task21;
 //Easy         
             
 //You are given the heads of two sorted linked lists list1 and list2.   
-   
+      
 //Merge the two lists in a one sorted list.       
 //The list should be made by splicing together the nodes of the first two lists.
 
