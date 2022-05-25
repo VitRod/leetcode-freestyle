a8f96bff2d8e31e4a9c48c396bd471a1acd8268e
VitRod <ukrdao@gmail.com>
2022-05-25 08:05:26 +0300

    formatting

--- src/main/java/com/task26/Solution26.java
+++ src/main/java/com/task26/Solution26.java
@@ -5,7 +5,7 @@ package com.task26;
     
 //Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such 
 //that each unique element appears only once. The relative order of the elements should be kept the same.
-
+    
 //Since it is impossible to change the length of the array in some languages,
 //you must instead have the result be placed in the first part of the array nums.
 //More formally, if there are k elements after removing the duplicates, 
