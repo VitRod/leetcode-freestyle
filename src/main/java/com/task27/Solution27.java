435a689c7f113d5d1add0b53b427cdbbd0228011
VitRod <ukrdao@gmail.com>
2022-05-26 04:08:08 +0300

    added files

--- src/main/java/com/task27/Solution27.java
+++ src/main/java/com/task27/Solution27.java
@@ -40,7 +40,7 @@ package com.task27;
 //Explanation: Your function should return k = 2, with the first two elements of nums being 2.
 //It does not matter what you leave beyond the returned k (hence they are underscores).
 
-//Example 2:
+//Example 2:    
 //Input: nums = [0,1,2,2,3,0,4,2], val = 2
 //Output: 5, nums = [0,1,4,0,3,_,_,_]
 //Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
