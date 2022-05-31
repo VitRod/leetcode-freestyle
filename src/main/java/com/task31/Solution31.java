2aa283484b852efb19be774398bb470f469430fb
VitRod <ukrdao@gmail.com>
2022-05-31 10:58:17 +0300

    added several files

--- src/main/java/com/task31/Solution31.java
+++ src/main/java/com/task31/Solution31.java
@@ -1,6 +1,6 @@
 package com.task31;
     
-//31. Next Permutation         
+//31. Next Permutation            
 //Medium                    
    
 //A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
