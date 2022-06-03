a4960866aa9751e06ade20d0153a0e1eafbfb426
VitRod <ukrdao@gmail.com>
2022-06-03 08:58:15 +0300

    added solution

--- src/main/java/com/task46/Solution46.java
+++ src/main/java/com/task46/Solution46.java
@@ -10,7 +10,7 @@ import java.util.List;
 // Given an array nums of distinct integers, return all the possible permutations. 
 // You can return the answer in any order.
    
-// Example 1:
+// Example 1:    
 
 // Input: nums = [1,2,3]
 // Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
