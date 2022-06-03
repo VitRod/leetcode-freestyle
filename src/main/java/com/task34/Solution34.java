7921a3f99227f3d02967b824e039723dd261bf0f
VitRod <ukrdao@gmail.com>
2022-06-04 01:36:30 +0300

    change committed

--- src/main/java/com/task34/Solution34.java
+++ src/main/java/com/task34/Solution34.java
@@ -1,7 +1,7 @@
 package com.task34;
 
 //34. Find First and Last Position of Element in Sorted Array
-//Medium           
+//Medium              
 
 //Given an array of integers nums sorted in non-decreasing order,
 //find the starting and ending position of a given target value.
