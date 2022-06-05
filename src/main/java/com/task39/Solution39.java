fc3ca1d0b7fb2f2b89560fc16d7685d3c7ee244f
VitRod <ukrdao@gmail.com>
2022-06-05 08:44:00 +0300

    formatting

--- src/main/java/com/task39/Solution39.java
+++ src/main/java/com/task39/Solution39.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 // 39. Combination Sum   
 // Medium      
-    
+        
 // Given an array of distinct integers candidates and a target integer target, 
 // return a list of all unique combinations of candidates where the chosen numbers sum to target.
 //  You may return the combinations in any order.
