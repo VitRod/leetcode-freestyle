9bd6ef28eaa36f41be5bf25b261e15cd6b907878
VitRod <ukrdao@gmail.com>
2022-06-09 09:40:15 +0300

    change committed

--- src/main/java/com/task56/Solution56.java
+++ src/main/java/com/task56/Solution56.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 
 // 56. Merge Intervals
 // Medium           
-
+    
 // Given an array of intervals where intervals[i] = [starti, endi], 
 // merge all overlapping intervals, and return an array of the non-overlapping 
 // intervals that cover all the intervals in the input.
