2eb7bbf30e47562b4a86fa5cd51988331d478110
VitRod <ukrdao@gmail.com>
2022-06-09 09:40:49 +0300

    change committed

--- src/main/java/com/task56/Solution56.java
+++ src/main/java/com/task56/Solution56.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 // merge all overlapping intervals, and return an array of the non-overlapping 
 // intervals that cover all the intervals in the input.
 
-// Example 1:
+// Example 1:   
 
 // Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 // Output: [[1,6],[8,10],[15,18]]
