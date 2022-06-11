ed8b1a3cd2e237ebefd3ef8936a4803031db8c57
VitRod <ukrdao@gmail.com>
2022-06-11 09:00:36 +0300

    added files

--- src/main/java/com/task70/Solution70.java
+++ src/main/java/com/task70/Solution70.java
@@ -3,7 +3,7 @@ package com.task70;
 //70. Climbing Stairs    
 //Easy                              
           
-//You are climbing a staircase. It takes n steps to reach the top.
+//You are climbing a staircase. It takes n steps to reach the top.     
 
 //Each time you can either climb 1 or 2 steps. 
 //In how many distinct ways can you climb to the top?
