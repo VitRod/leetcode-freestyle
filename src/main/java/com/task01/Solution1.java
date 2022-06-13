8ebb1412480e774e03f6a68c3f1b1ee6b1afcaf6
VitRod <ukrdao@gmail.com>
2022-06-14 01:16:31 +0300

    added comments to code

--- src/main/java/com/task01/Solution1.java
+++ src/main/java/com/task01/Solution1.java
@@ -1,6 +1,6 @@
 package com.task01;
                   
-//1. Two Sum        
+//1. Two Sum            
 //Easy                         
 //             
 //Given an array of integers nums and an integer target, 
