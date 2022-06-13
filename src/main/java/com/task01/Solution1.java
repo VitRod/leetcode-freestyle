0ed05fba67e73c74bb73b9998e975efa49d58cd3
VitRod <ukrdao@gmail.com>
2022-06-14 01:16:20 +0300

    added comments to code

--- src/main/java/com/task01/Solution1.java
+++ src/main/java/com/task01/Solution1.java
@@ -1,7 +1,7 @@
 package com.task01;
                   
 //1. Two Sum        
-//Easy                     
+//Easy                         
 //             
 //Given an array of integers nums and an integer target, 
 //return indices of the two numbers such that they add up to target.
