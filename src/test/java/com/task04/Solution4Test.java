f2905375788d2c1a6664eb582cb511ed9e6250c4
VitRod <ukrdao@gmail.com>
2022-06-13 07:47:00 +0300

    made a  commit

--- src/test/java/com/task04/Solution4Test.java
+++ src/test/java/com/task04/Solution4Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class Solution4Test {
 
 	Solution4 s = new Solution4();
-
+   
 	@Test
 	public void test1() {
 		assertEquals(2.0, s.findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
