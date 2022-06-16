7cd92b5153414bc19e42f4d1b27bfb51971c2f6c
VitRod <ukrdao@gmail.com>
2022-06-16 05:19:57 +0300

    added comments to code

--- src/test/java/com/task10/Solution10Test.java
+++ src/test/java/com/task10/Solution10Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;   
 
-class Solution10Test {    
+class Solution10Test {       
      
 	@Test 
     public void testIsMatch() {
