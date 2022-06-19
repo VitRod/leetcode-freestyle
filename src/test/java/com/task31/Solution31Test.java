616970d17f88a2bb4ffc88e14a12cf3205c445c5
VitRod <ukrdao@gmail.com>
2022-06-20 02:58:14 +0300

    change committed

--- src/test/java/com/task31/Solution31Test.java
+++ src/test/java/com/task31/Solution31Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
            
 import org.junit.jupiter.api.Test;
-   
+       
 class Solution31Test {
 	Solution31 solution = new Solution31();
 
