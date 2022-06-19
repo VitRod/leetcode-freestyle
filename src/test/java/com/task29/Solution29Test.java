8e6d6205c19b4c216eab601181dec3598509f8d3
VitRod <ukrdao@gmail.com>
2022-06-19 05:05:42 +0300

    commit solution

--- src/test/java/com/task29/Solution29Test.java
+++ src/test/java/com/task29/Solution29Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
         
 class Solution29Test {     
-    
+        
 	Solution29 solution = new Solution29();
    
 	@Test
