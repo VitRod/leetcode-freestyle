5b920a549e7c9f4eb3c0b2f77556e5ff1eec73a5
VitRod <ukrdao@gmail.com>
2022-06-21 06:49:08 +0300

    commit solution

--- src/test/java/com/task34/Solution34Test.java
+++ src/test/java/com/task34/Solution34Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
            
 class Solution34Test {   
-         
+             
 	Solution34 solution = new Solution34();
    
 	@Test
