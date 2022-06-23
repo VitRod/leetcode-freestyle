0377d2d56223da220341bbe6781e71929e82c4b3
VitRod <ukrdao@gmail.com>
2022-06-23 06:49:44 +0300

    added comments to code

--- src/test/java/com/task50/Solution50Test.java
+++ src/test/java/com/task50/Solution50Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;     
                          
-class Solution50Test {
+class Solution50Test {    
     
 	Solution50 solution = new Solution50();
 
