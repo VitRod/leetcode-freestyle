dc9d3d5cf94852bacae670bc6c18909024c05701
VitRod <ukrdao@gmail.com>
2022-06-25 07:11:31 +0300

    changes

--- src/test/java/com/task63/Solution63Test.java
+++ src/test/java/com/task63/Solution63Test.java
@@ -13,7 +13,7 @@ class Solution63Test {
 		int[][] grid = new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
 		assertEquals(2, s.uniquePathsWithObstacles(grid));
 	}
-
+   
 	@Test
 	public void test2() {
 		int[][] grid = new int[][] { { 0, 1 }, { 0, 0 } };
