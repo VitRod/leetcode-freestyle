621823378eff5c10f9c81ed77722970299591cab
VitRod <ukrdao@gmail.com>
2022-07-03 06:55:08 +0300

    little changes

--- src/test/java/com/task69/Solution69Test.java
+++ src/test/java/com/task69/Solution69Test.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Solution69Test {
-         
+             
 	Solution69 s = new Solution69();
         
 	@Test
