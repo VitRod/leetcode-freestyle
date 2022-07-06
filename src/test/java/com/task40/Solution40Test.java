14f7fe63ea1074b3ead6f19612bf5a3008e59d67
VitRod <ukrdao@gmail.com>
2022-07-06 05:53:14 +0300

    added comments to code

--- src/test/java/com/task40/Solution40Test.java
+++ src/test/java/com/task40/Solution40Test.java
@@ -3,7 +3,7 @@ package com.task40;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.List;    
 
 import org.junit.jupiter.api.Test;
 
