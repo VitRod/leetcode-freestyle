cc1187759ca9110e9c42fc125b8d26db99790efc
VitRod <ukrdao@gmail.com>
2022-07-06 05:54:18 +0300

    added comments to code

--- src/test/java/com/task40/Solution40Test.java
+++ src/test/java/com/task40/Solution40Test.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;    
     
 import org.junit.jupiter.api.Test;
-            
+               
 class Solution40Test {
     
 	Solution40 solution = new Solution40();
