db6be850bb49ef9fd0a4908fde539344d28ea69e
VitRod <ukrdao@gmail.com>
2022-07-07 08:28:48 +0300

    commit tests

--- src/test/java/com/task49/Solution49Test.java
+++ src/test/java/com/task49/Solution49Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 import java.util.List;
-         
+             
 import org.junit.jupiter.api.Test;
         
 class Solution49Test {
