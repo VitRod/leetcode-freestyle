a386e0ee5811af8e770c9a37ff0868a5b070a452
VitRod <ukrdao@gmail.com>
2022-07-11 02:24:25 +0300

    added comments to code

--- src/test/java/com/task74/Solution74Test.java
+++ src/test/java/com/task74/Solution74Test.java
@@ -15,7 +15,7 @@ class Solution74Test {
 		boolean actual = s.searchMatrix(matrix, target);
 		assertEquals(expected, actual);
 	}
-
+   
 	@Test
 	public void test2() {
 		int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
