901deefa6eb522765f9465e9c7485b5a18d61f5b
VitRod <ukrdao@gmail.com>
2022-07-11 02:25:11 +0300

    added comments to code

--- src/test/java/com/task74/Solution74Test.java
+++ src/test/java/com/task74/Solution74Test.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class Solution74Test {
     
 	Solution74 s = new Solution74();
-                              
+                                 
 	@Test
 	public void test1() {
 		int[][] matrix = new int[][] { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
