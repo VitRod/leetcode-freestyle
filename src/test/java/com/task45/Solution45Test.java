02d270e168344ce8c7ba614e9996cd226dcb1bb9
VitRod <ukrdao@gmail.com>
2022-07-10 07:46:42 +0300

    made a commit

--- src/test/java/com/task45/Solution45Test.java
+++ src/test/java/com/task45/Solution45Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class Solution45Test {
             
 	Solution45 solution = new Solution45();
-             
+                 
 	@Test
 	public void test1() {
 		int[] nums = { 2, 3, 1, 1, 4 };
