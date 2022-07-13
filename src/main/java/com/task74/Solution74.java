e289050eea4eca2d97d335d124d0049ab19e6f38
VitRod <ukrdao@gmail.com>
2022-07-13 05:27:43 +0300

    modified the  code

--- src/main/java/com/task74/Solution74.java
+++ src/main/java/com/task74/Solution74.java
@@ -8,7 +8,7 @@ package com.task74;
 
 //Integers in each row are sorted from left to right.
 //The first integer of each row is greater than the last integer of the previous row.
-
+   
 //Example 1:
 
 //Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
