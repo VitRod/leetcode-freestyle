efa17eaf4dde5a597d665d58341be8bb44c46449
VitRod <ukrdao@gmail.com>
2022-07-16 07:31:34 +0300

    commit code

--- src/main/java/com/task19/Solution19.java
+++ src/main/java/com/task19/Solution19.java
@@ -4,7 +4,7 @@ package com.task19;
 //Medium
    
 //Given the head of a linked list, remove the nth node from the end of the list and return its head.
-               
+                  
 //Example 1:                 
 
 //Input: head = [1,2,3,4,5], n = 2        
