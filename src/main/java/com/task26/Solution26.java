9e70d7f36ba19f0045a92b80d501e1b0f6fcbe3c
VitRod <ukrdao@gmail.com>
2022-07-18 06:51:27 +0300

    made commit

--- src/main/java/com/task26/Solution26.java
+++ src/main/java/com/task26/Solution26.java
@@ -30,7 +30,7 @@ package com.task26;
 //for (int i = 0; i < k; i++) {
 //  assert nums[i] == expectedNums[i];
 //}
-//If all assertions pass, then your solution will be accepted.
+//If all assertions pass, then your solution will be accepted.   
 
 //Example 1:
 //Input: nums = [1,1,2]
