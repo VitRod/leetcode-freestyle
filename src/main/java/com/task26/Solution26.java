9bc87c228f783f6284fff894fad6eb4796da786b
VitRod <ukrdao@gmail.com>
2022-07-18 06:50:49 +0300

    made commit

--- src/main/java/com/task26/Solution26.java
+++ src/main/java/com/task26/Solution26.java
@@ -2,7 +2,7 @@ package com.task26;
 
 //26. Remove Duplicates from Sorted Array          
 //Easy                       
-                       
+                          
 //Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such 
 //that each unique element appears only once. The relative order of the elements should be kept the same.
     
