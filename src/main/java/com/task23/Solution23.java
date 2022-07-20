647719a0908be8af2b2769bbebc056830de3ee04
VitRod <ukrdao@gmail.com>
2022-07-20 07:06:05 +0300

    made commit

--- src/main/java/com/task23/Solution23.java
+++ src/main/java/com/task23/Solution23.java
@@ -2,7 +2,7 @@ package com.task23;
 
 //23. Merge k Sorted Lists
 //Hard                   
-                                           
+                                                  
 //You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
 //Merge all the linked-lists into one sorted linked-list and return it.      
