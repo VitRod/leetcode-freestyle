0ac170511b0f5d60120115c32cf315bd092a3a47
VitRod <ukrdao@gmail.com>
2022-07-21 09:11:56 +0300

    modified code

--- src/main/java/com/task21/Solution21.java
+++ src/main/java/com/task21/Solution21.java
@@ -1,7 +1,7 @@
 package com.task21;
 
 //21. Merge Two Sorted Lists   
-//Easy         
+//Easy              
                                              
 //You are given the heads of two sorted linked lists list1 and list2.   
       
