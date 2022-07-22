7687bf36c590769838840e1e1f1f63863477d610
VitRod <ukrdao@gmail.com>
2022-07-22 11:13:51 +0300

    added code

--- src/main/java/com/task38/Solution38.java
+++ src/main/java/com/task38/Solution38.java
@@ -4,7 +4,7 @@ package com.task38;
 //Medium
              
 //The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
-          
+              
 //countAndSay(1) = "1"
 //countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1),
 //which is then converted into a different digit string.
