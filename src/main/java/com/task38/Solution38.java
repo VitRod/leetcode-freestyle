8c31064fdb77e3fac2c6d4c94d5035fe3ed49b6d
VitRod <ukrdao@gmail.com>
2022-07-22 11:12:58 +0300

    added code

--- src/main/java/com/task38/Solution38.java
+++ src/main/java/com/task38/Solution38.java
@@ -12,7 +12,7 @@ package com.task38;
 //so that each group is a contiguous section all of the same character. Then for each group, 
 //say the number of characters, then say the character. To convert the saying into a digit string,
 //replace the counts with a number and concatenate every saying.
-
+    
 //For example, the saying and conversion for digit string "3322251":
 
 //Given a positive integer n, return the nth term of the count-and-say sequence.
