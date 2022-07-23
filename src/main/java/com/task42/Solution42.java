f282971f7e55bd8431a08da4eaf8b07cc4b83688
VitRod <ukrdao@gmail.com>
2022-07-23 04:17:55 +0300

    modified code

--- src/main/java/com/task42/Solution42.java
+++ src/main/java/com/task42/Solution42.java
@@ -6,7 +6,7 @@ package com.task42;
 //Given n non-negative integers representing an elevation map where the width of each bar is 1,
 //compute how much water it can trap after raining.
     
-       
+            
 
 //Example 1:
 
