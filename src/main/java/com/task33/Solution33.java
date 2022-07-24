e058e63d6f62f390500d123463279b68b5c32bf2
VitRod <ukrdao@gmail.com>
2022-07-24 09:46:49 +0300

    modified  code

--- src/main/java/com/task33/Solution33.java
+++ src/main/java/com/task33/Solution33.java
@@ -2,7 +2,7 @@ package com.task33;
 
 //33. Search in Rotated Sorted Array                     
 //Medium                                                       
-                               
+                                  
 //There is an integer array nums sorted in ascending order (with distinct values).          
 
 //Prior to being passed to your function, nums is possibly rotated at an
