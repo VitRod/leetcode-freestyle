9dc9dcdcbb62262a246e6ea9e427421e8fd189e1
VitRod <ukrdao@gmail.com>
2022-07-26 06:41:13 +0300

    modified code

--- src/main/java/com/task88/Solution88.java
+++ src/main/java/com/task88/Solution88.java
@@ -2,7 +2,7 @@ package com.task88;
 
 //88. Merge Sorted Array
 //Easy
-                            
+                                
 //You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
 //and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
