9fb3f53a873483b18cf3d78d8048679e0dd9747a
VitRod <ukrdao@gmail.com>
2022-07-26 06:41:51 +0300

    modified code

--- src/main/java/com/task88/Solution88.java
+++ src/main/java/com/task88/Solution88.java
@@ -5,7 +5,7 @@ package com.task88;
                                     
 //You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
 //and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-      
+          
 //Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 //The final sorted array should not be returned by the function,
