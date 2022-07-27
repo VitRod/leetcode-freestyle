ea492548e159ec78a5ad5759437611e6a79b5f85
VitRod <ukrdao@gmail.com>
2022-07-27 04:32:41 +0300

    modified  code

--- src/main/java/com/task59/Solution59.java
+++ src/main/java/com/task59/Solution59.java
@@ -5,7 +5,7 @@ package com.task59;
        
 //Given a positive integer n, generate an n x n matrix filled 
 //with elements from 1 to n2 in spiral order.
-
+    
 //Example 1:
 //Input: n = 3
 //Output: [[1,2,3],[8,9,4],[7,6,5]]
