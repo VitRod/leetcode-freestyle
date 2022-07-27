16531826b8ec1c33e3c9689b57aeb17345996763
VitRod <ukrdao@gmail.com>
2022-07-27 04:33:13 +0300

    modified  code

--- src/main/java/com/task59/Solution59.java
+++ src/main/java/com/task59/Solution59.java
@@ -2,7 +2,7 @@ package com.task59;
 
 //59. Spiral Matrix II
 //Medium
-                 
+                    
 //Given a positive integer n, generate an n x n matrix filled 
 //with elements from 1 to n2 in spiral order.
     
