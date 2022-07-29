47aa02baab1ceb94c8207ee485f4b7e534df5e74
VitRod <ukrdao@gmail.com>
2022-07-29 04:36:39 +0300

    modified  code

--- src/main/java/com/task61/Solution61.java
+++ src/main/java/com/task61/Solution61.java
@@ -4,7 +4,7 @@ package com.task61;
 //Medium
         
 //Given the head of a linked list, rotate the list to the right by k places.
-
+    
 //Example 1:
 //Input: head = [1,2,3,4,5], k = 2
 //Output: [4,5,1,2,3]
