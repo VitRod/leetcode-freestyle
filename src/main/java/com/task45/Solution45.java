c2c51792459bae8598c633a1d68bdc4293994cab
VitRod <ukrdao@gmail.com>
2022-07-30 08:29:37 +0300

    modified  code

--- src/main/java/com/task45/Solution45.java
+++ src/main/java/com/task45/Solution45.java
@@ -5,7 +5,7 @@ package com.task45;
            
 //Given an array of non-negative integers nums, you are initially 
 //positioned at the first index of the array.
-
+    
 //Each element in the array represents your maximum jump length at that position.
     
 //Your goal is to reach the last index in the minimum number of jumps.
