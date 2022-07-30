1c787dfb17f4a906678adeb3211f160f94e5f2fa
VitRod <ukrdao@gmail.com>
2022-07-30 08:29:27 +0300

    modified  code

--- src/main/java/com/task45/Solution45.java
+++ src/main/java/com/task45/Solution45.java
@@ -7,7 +7,7 @@ package com.task45;
 //positioned at the first index of the array.
 
 //Each element in the array represents your maximum jump length at that position.
-
+    
 //Your goal is to reach the last index in the minimum number of jumps.
 
 //You can assume that you can always reach the last index.
