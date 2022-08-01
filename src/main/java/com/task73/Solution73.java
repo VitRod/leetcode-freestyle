f2b603c087c7bd16f587e0a998029adafe014360
VitRod <ukrdao@gmail.com>
2022-08-01 06:29:06 +0300

    modifications

--- src/main/java/com/task73/Solution73.java
+++ src/main/java/com/task73/Solution73.java
@@ -2,7 +2,7 @@ package com.task73;
 
 //73. Set Matrix Zeroes
 //Medium
-
+    
 //Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
                   
 //You must do it in place.
