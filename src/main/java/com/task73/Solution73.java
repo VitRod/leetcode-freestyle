239fbc708d4389b7ba25d1625d5047f9dbe2ee95
VitRod <ukrdao@gmail.com>
2022-08-01 06:27:54 +0300

    modifications

--- src/main/java/com/task73/Solution73.java
+++ src/main/java/com/task73/Solution73.java
@@ -6,7 +6,7 @@ package com.task73;
 //Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
 //You must do it in place.
-
+    
 //Example 1:
 
 //Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
