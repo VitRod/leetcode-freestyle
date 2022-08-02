b6be228b6e6074c61da26b248da7bdc3de3caedd
VitRod <ukrdao@gmail.com>
2022-08-02 07:42:07 +0300

    modifications

--- src/main/java/com/task83/Solution83.java
+++ src/main/java/com/task83/Solution83.java
@@ -5,7 +5,7 @@ package com.task83;
 
 //Given the head of a sorted linked list, delete all duplicates 
 //such that each element appears only once. Return the linked list sorted as well.
-        
+            
 //Example 1:
 //Input: head = [1,1,2]
 //Output: [1,2]
