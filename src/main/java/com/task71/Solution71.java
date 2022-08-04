c9c41474b94900d55260767212ba65c31d014689
VitRod <ukrdao@gmail.com>
2022-08-04 06:57:08 +0300

    edited code

--- src/main/java/com/task71/Solution71.java
+++ src/main/java/com/task71/Solution71.java
@@ -8,7 +8,7 @@ import java.util.Set;
                      
 // 71. Simplify Path
 // Medium
-           
+               
 // Given a string path, which is an absolute path (starting with a slash '/')
 //  to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
 
