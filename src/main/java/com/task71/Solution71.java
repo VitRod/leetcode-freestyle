403b771471fa9864c7b1b4f9ce397d19c276481c
VitRod <ukrdao@gmail.com>
2022-08-04 06:55:38 +0300

    edited code

--- src/main/java/com/task71/Solution71.java
+++ src/main/java/com/task71/Solution71.java
@@ -27,7 +27,7 @@ import java.util.Set;
 // Return the simplified canonical path.
 
 // Example 1:
-
+   
 // Input: path = "/home/"
 // Output: "/home"
 // Explanation: Note that there is no trailing slash after the last directory name.
