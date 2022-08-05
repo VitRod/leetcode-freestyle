36c85ba3a938387a6062b1fad97f0b123bfc2415
VitRod <ukrdao@gmail.com>
2022-08-05 08:37:46 +0300

    edited code

--- src/main/java/com/task67/Solution67.java
+++ src/main/java/com/task67/Solution67.java
@@ -5,7 +5,7 @@ package com.task67;
     
 //Given two binary strings a and b, return their sum as a binary string.
     
-//Example 1:    
+//Example 1:        
 
 //Input: a = "11", b = "1"
 //Output: "100"
