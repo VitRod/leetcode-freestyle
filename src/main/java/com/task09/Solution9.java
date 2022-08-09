88e3f2a736547187253b70508a51e0e89f5ceed4
VitRod <ukrdao@gmail.com>
2022-08-09 06:58:34 +0300

    edited code

--- src/main/java/com/task09/Solution9.java
+++ src/main/java/com/task09/Solution9.java
@@ -8,7 +8,7 @@ package com.task09;
 //An integer is a palindrome when it reads the same backward as forward.
 
 //For example, 121 is a palindrome while 123 is not.
-
+    
         
 //Example 1:
 
