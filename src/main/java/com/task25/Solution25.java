b36a319ffac0727ca9acb0988834e33c692d7902
VitRod <ukrdao@gmail.com>
2023-04-04 15:56:40 +0300

    task text solution 25

--- src/main/java/com/task25/Solution25.java
+++ src/main/java/com/task25/Solution25.java
@@ -1,5 +1,37 @@
 package com.task25;
 
+/*
+25. Reverse Nodes in k-Group
+Hard
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list.
+If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+Example 2:
+
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+
+ */
+
+
+
+
 class Solution25 {
     public ListNode reverseKGroup(ListNode head, int k) {
         if (head == null || k == 1) {
